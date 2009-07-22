51d445a027d60ab400a213d368a004c9ae9fedfa
Simone Carletti <weppos@weppos.net>
2009-07-22 11:07:38 +0200

    Skip online test when no connectivity is available.

--- test/client_test.rb
+++ test/client_test.rb
@@ -10,13 +10,7 @@ class ClientTest < Test::Unit::TestCase
   def test_query_with_email_address_should_raise
     assert_raise(ServerNotSupported) { @client.query("weppos@weppos.net") }
   end
-  
-  def test_query_with_domain
-    response = @client.query("weppos.it")
-    assert_match /Domain:\s+weppos\.it/, response
-    assert_match /Created:/, response
-  end
-  
+
   def test_query_with_domain_with_no_whois
     error = assert_raise(NoInterfaceError) { @client.query("weppos.ad") }
     assert_match /no whois server/, error.message
@@ -27,5 +21,16 @@ class ClientTest < Test::Unit::TestCase
     assert_match /no whois server/, error.message
     assert_match /www\.nic\.ar/, error.message
   end
-  
+
+
+  need_connectivity do
+
+    def test_query_with_domain
+      response = @client.query("weppos.it")
+      assert_match /Domain:\s+weppos\.it/, response
+      assert_match /Created:/, response
+    end
+
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,29 @@ require 'rubygems'
 require 'test/unit'
 require 'mocha'
 require 'whois'
+
+
+module ConnectivityTestHelper
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+
+    def need_connectivity(&block)
+      if connectivity_available?
+        yield
+      end
+    end
+
+    def connectivity_available?
+      ENV["ONLINE"].to_i == 1
+    end
+
+  end
+end
+
+
+class Test::Unit::TestCase
+  include ConnectivityTestHelper
+end
