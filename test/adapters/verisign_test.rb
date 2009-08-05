d1cff12cc119c78cd577bfd30d9c39af113504bc
Simone Carletti <weppos@weppos.net>
2009-08-05 18:00:53 +0200

    Whois::Server::Adapters::Verisign adapter always use "whois.crsnic.net" regardless the adapter set in the server definition.

--- test/adapters/verisign_test.rb
+++ test/adapters/verisign_test.rb
@@ -11,14 +11,14 @@ class ServerAdaptersVerisignTest < Test::Unit::TestCase
 
   def test_query
     expected = "No match for DOMAIN.FOO."
-    @server.expects(:ask_the_socket).with("=domain.foo", "whois.crsnic.net", 43).returns(expected)
+    @server.expects(:ask_the_socket).with("=domain.foo", "whois.foo", 43).returns(expected)
     assert_equal expected, @server.query("domain.foo")
   end
 
   def test_query_with_referral
     response = File.read(File.dirname(__FILE__) + "/../testcases/referrals/crsnic.com.txt")
     expected = "Match for DOMAIN.FOO."
-    @server.expects(:ask_the_socket).with("=domain.foo", "whois.crsnic.net", 43).returns(response)
+    @server.expects(:ask_the_socket).with("=domain.foo", "whois.foo", 43).returns(response)
     @server.expects(:ask_the_socket).with("domain.foo", "whois.tucows.com", 43).returns(expected)
     assert_equal expected, @server.query("domain.foo")
   end
