3a23711f03492a31a192a4e0c7b3d52266d1a6fc
Simone Carletti <weppos@weppos.net>
2009-11-11 00:45:48 +0100

    Added Base .fr TLD parser.

---
+++ lib/whois/answer/parser/whois.nic.fr.rb
@@ -0,0 +1,78 @@
+#
+# = Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+#
+# Category::    Net
+# Package::     Whois
+# Author::      Simone Carletti <weppos@weppos.net>
+# License::     MIT License
+#
+#--
+#
+#++
+
+
+require 'whois/answer/parser/base'
+
+
+module Whois
+  class Answer
+    class Parser
+
+      #
+      # = whois.nic.fr parser
+      #
+      # Parser for the whois.nic.fr server.
+      #
+      # NOTE: This parser is just a stub and provides only a few basic methods
+      # to check for domain availability and get domain status.
+      # Please consider to contribute implementing missing methods.
+      # See WhoisNicIt parser for an explanation of all available methods
+      # and examples.
+      #
+      class WhoisNicFr < Base
+
+
+        register_method :status do
+          @status ||= if content.to_s =~ /status:\s+(.*)\n/
+            $1.downcase.to_sym
+          else
+            :available
+          end
+        end
+
+        register_method :available? do
+          @available ||= !!(content.to_s =~ /No entries found in the AFNIC Database/)
+        end
+
+        register_method :registered? do
+          !available?
+        end
+
+
+        register_method :created_on do
+          @created_on ||= if content.to_s =~ /created:\s+(.*)\n/
+            d, m, y = $1.split("/")
+            Time.parse("#{y}-#{m}-#{d}")
+          end
+        end
+        
+        register_method :updated_on do
+          @created_on ||= if content.to_s =~ /last-update:\s+(.*)\n/
+            d, m, y = $1.split("/")
+            Time.parse("#{y}-#{m}-#{d}")
+          end
+        end
+        
+        # FIXME: implement? && !support? (or anniversary?)
+        register_method :expires_on do
+          nil
+        end
+
+      end
+      
+    end
+  end
+end  
---
+++ test/answer/parser/whois_nic_fr_test.rb
@@ -0,0 +1,75 @@
+require 'test_helper'
+require 'whois/answer/parser/whois.nic.fr.rb'
+
+class AnswerParserWhoisNicFrTest < Test::Unit::TestCase
+
+  TESTCASE_PATH = File.expand_path(File.dirname(__FILE__) + '/../../testcases/responses/whois.nic.fr')
+
+  def setup
+    @klass  = Whois::Answer::Parser::WhoisNicFr
+    @host   = "whois.nic.fr"
+  end
+
+
+
+
+  def test_status
+    assert_equal  :available,
+                  @klass.new(load_part('/available.txt')).status
+    assert_equal  :active,
+                  @klass.new(load_part('/registered.txt')).status
+  end
+
+  def test_available?
+    assert !@klass.new(load_part('/registered.txt')).available?
+    assert  @klass.new(load_part('/available.txt')).available?
+  end
+
+  def test_registered?
+    assert !@klass.new(load_part('/available.txt')).registered?
+    assert  @klass.new(load_part('/registered.txt')).registered?
+  end
+
+
+  def test_created_on
+    assert_equal  Time.parse("2000-07-27"),
+                  @klass.new(load_part('/registered.txt')).created_on
+  end
+
+  def test_created_on_with_available
+    assert_equal  nil,
+                  @klass.new(load_part('/available.txt')).created_on
+  end
+
+  def test_updated_on
+    assert_equal  Time.parse("2009-06-03"),
+                  @klass.new(load_part('/registered.txt')).updated_on
+  end
+
+  def test_updated_on_with_available
+    assert_equal  nil,
+                  @klass.new(load_part('/available.txt')).updated_on
+  end
+
+  def test_expires_on
+    assert_equal  nil,
+                  @klass.new(load_part('/registered.txt')).expires_on
+  end
+
+  def test_expires_on_with_available
+    assert_equal  nil,
+                  @klass.new(load_part('/available.txt')).expires_on
+  end
+
+
+  protected
+
+    def load_part(path)
+      part(File.read(TESTCASE_PATH + path), @host)
+    end
+
+    def part(*args)
+      Whois::Answer::Part.new(*args)
+    end
+
+end
