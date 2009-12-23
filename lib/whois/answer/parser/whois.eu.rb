394355bd2857fc97a15bc16acdbb05b4a1c05bc4
Simone Carletti <weppos@weppos.net>
2009-12-23 16:03:24 +0100

    Added Simple .eu TLD parser.

---
+++ lib/whois/answer/parser/whois.eu.rb
@@ -0,0 +1,73 @@
+#
+# = Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+#
+# Category::    Net
+# Package::     Whois
+# Author::      Simone Carletti <weppos@weppos.net>
+# License::     MIT License
+#
+#--
+#
+#++
+
+
+require 'whois/answer/parser/base'
+
+
+module Whois
+  class Answer
+    class Parser
+
+      #
+      # = whois.eu parser
+      #
+      # Parser for the whois.eu server.
+      #
+      # NOTE: This parser is just a stub and provides only a few basic methods
+      # to check for domain availability and get domain status.
+      # Please consider to contribute implementing missing methods.
+      # See WhoisNicIt parser for an explanation of all available methods
+      # and examples.
+      #
+      class WhoisEu < Base
+
+        register_method :status do
+          if available?
+            :available
+          else
+            :registered
+          end
+        end
+
+        register_method :available? do
+          @available ||= !!(content.to_s =~ /Status:\s+AVAILABLE/)
+        end
+
+        register_method :registered? do
+          !available?
+        end
+
+
+        # TODO: NotAvailable
+        register_method :created_on do
+          nil
+        end
+        
+        # TODO: NotAvailable
+        register_method :updated_on do
+          nil
+        end
+        
+        # TODO: NotAvailable
+        register_method :expires_on do
+          nil
+        end
+
+      end
+      
+    end
+  end
+end  
---
+++ test/answer/parser/whois.eu_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+require 'whois/answer/parser/whois.eu.rb'
+
+class AnswerParserWhoisEuTest < Whois::Answer::Parser::TestCase
+
+  def setup
+    @klass  = Whois::Answer::Parser::WhoisEu
+    @host   = "whois.eu"
+  end
+
+
+  def test_status
+    assert_equal  :available,
+                  @klass.new(load_part('/available.txt')).status
+    assert_equal  :registered,
+                  @klass.new(load_part('/registered.txt')).status
+  end
+
+  def test_available?
+    assert !@klass.new(load_part('/registered.txt')).available?
+    assert  @klass.new(load_part('/available.txt')).available?
+  end
+
+  def test_registered?
+    assert !@klass.new(load_part('/available.txt')).registered?
+    assert  @klass.new(load_part('/registered.txt')).registered?
+  end
+
+
+  def test_created_on
+    assert_equal  nil,
+                  @klass.new(load_part('/registered.txt')).created_on
+    assert_equal  nil,
+                  @klass.new(load_part('/available.txt')).created_on
+  end
+
+  def test_updated_on
+    assert_equal  nil,
+                  @klass.new(load_part('/registered.txt')).updated_on
+    assert_equal  nil,
+                  @klass.new(load_part('/available.txt')).updated_on
+  end
+
+  def test_expires_on
+    assert_equal  nil,
+                  @klass.new(load_part('/registered.txt')).expires_on
+    assert_equal  nil,
+                  @klass.new(load_part('/available.txt')).expires_on
+  end
+
+end
