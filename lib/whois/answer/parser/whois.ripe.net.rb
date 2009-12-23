a50616d5ecbad1b62ab3c4f5d54e6f0d7646ca60
Simone Carletti <weppos@weppos.net>
2009-12-23 18:10:20 +0100

    Added Simple .sm TLD parser.

---
+++ lib/whois/answer/parser/whois.ripe.net.rb
@@ -0,0 +1,73 @@
+#
+# = Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+#
+# Category::    Net
+# Package::     Whois
+# Author::      Simone Carletti <weppos@weppos.net>
+# License::     MIT License
+#
+#--
+#
+#++
+
+
+require 'whois/answer/parser/base'
+
+
+module Whois
+  class Answer
+    class Parser
+
+      #
+      # = whois.ripe.net parser
+      #
+      # Parser for the whois.ripe.net server.
+      #
+      # NOTE: This parser is just a stub and provides only a few basic methods
+      # to check for domain availability and get domain status.
+      # Please consider to contribute implementing missing methods.
+      # See WhoisNicIt parser for an explanation of all available methods
+      # and examples.
+      #
+      class WhoisRipeNet < Base
+
+        register_method :status do
+          if available?
+            :available
+          else
+            :registered
+          end
+        end
+
+        register_method :available? do
+          @available ||= !!(content.to_s =~ /no entries found/)
+        end
+
+        register_method :registered? do
+          !available?
+        end
+
+
+        # TODO: NotAvailable
+        register_method :created_on do
+          nil
+        end
+
+        # TODO: NotAvailable
+        register_method :updated_on do
+          nil
+        end
+
+        # TODO: NotAvailable
+        register_method :expires_on do
+          nil
+        end
+
+      end
+      
+    end
+  end
+end  
---
+++ test/testcases/responses/whois.ripe.net/sm/available.txt
@@ -0,0 +1,11 @@
+% This is the RIPE Database query service.
+% The objects are in RPSL format.
+%
+% The RIPE Database is subject to Terms and Conditions.
+% See http://www.ripe.net/db/support/db-terms-conditions.pdf
+
+%ERROR:101: no entries found
+%
+% No entries found in the selected source(s).
+
+
