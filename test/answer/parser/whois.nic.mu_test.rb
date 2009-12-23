474835372ab1b818cf0b6ece8746732deb3659e1
Simone Carletti <weppos@weppos.net>
2009-12-23 17:24:02 +0100

    Added Simple .mu TLD parser.

---
+++ test/answer/parser/whois.nic.mu_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+require 'whois/answer/parser/whois.nic.mu.rb'
+
+class AnswerParserWhoisNicMuTest < Whois::Answer::Parser::TestCase
+
+  def setup
+    @klass  = Whois::Answer::Parser::WhoisNicMu
+    @host   = "whois.nic.mu"
+  end
+
+
+  def test_status
+    assert_equal  :registered,
+                  @klass.new(load_part('/registered.txt')).status
+    assert_equal  :available,
+                  @klass.new(load_part('/available.txt')).status
+  end
+
+  def test_available?
+    assert !@klass.new(load_part('/registered.txt')).available?
+    assert  @klass.new(load_part('/available.txt')).available?
+  end
+
+  def test_registered?
+    assert !@klass.new(load_part('/available.txt')).registered?
+    assert  @klass.new(load_part('/registered.txt')).registered?
+  end
+
+
+  def test_created_on
+    assert_equal  Time.parse("2000-12-21"),
+                  @klass.new(load_part('/registered.txt')).created_on
+    assert_equal  nil,
+                  @klass.new(load_part('/available.txt')).created_on
+  end
+
+  def test_updated_on
+    assert_equal  Time.parse("2009-12-04"),
+                  @klass.new(load_part('/registered.txt')).updated_on
+    assert_equal  nil,
+                  @klass.new(load_part('/available.txt')).updated_on
+  end
+
+  def test_expires_on
+    assert_equal  Time.parse("2010-12-20"),
+                  @klass.new(load_part('/registered.txt')).expires_on
+    assert_equal  nil,
+                  @klass.new(load_part('/available.txt')).expires_on
+  end
+
+end
