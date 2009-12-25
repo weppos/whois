dff7f9a13047d717f34c364501e6899e6dbd9571
Simone Carletti <weppos@weppos.net>
2009-12-25 23:14:18 +0100

    Added simple .br parser.

---
+++ test/answer/parser/whois.registro.br_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+require 'whois/answer/parser/whois.registro.br.rb'
+
+class AnswerParserWhoisRegistroBrTest < Whois::Answer::Parser::TestCase
+
+  def setup
+    @klass  = Whois::Answer::Parser::WhoisRegistroBr
+    @host   = "whois.registro.br"
+  end
+
+
+  def test_status
+    assert_equal  :available,
+                  @klass.new(load_part('/available.txt')).status
+    assert_equal  :registered,
+                  @klass.new(load_part('/registered.txt')).status
+  end
+
+  def test_available?
+    assert !@klass.new(load_part('/registered.txt')).available?
+    assert  @klass.new(load_part('/available.txt')).available?
+  end
+
+  def test_registered?
+    assert !@klass.new(load_part('/available.txt')).registered?
+    assert  @klass.new(load_part('/registered.txt')).registered?
+  end
+
+
+  def test_created_on
+    assert_raise(Whois::PropertyNotSupported) { @klass.new(load_part('/registered.txt')).created_on }
+    assert_raise(Whois::PropertyNotSupported) { @klass.new(load_part('/available.txt')).created_on }
+  end
+
+  def test_updated_on
+    assert_raise(Whois::PropertyNotSupported) { @klass.new(load_part('/registered.txt')).updated_on }
+    assert_raise(Whois::PropertyNotSupported) { @klass.new(load_part('/available.txt')).updated_on }
+  end
+
+  def test_expires_on
+    assert_raise(Whois::PropertyNotSupported) { @klass.new(load_part('/registered.txt')).expires_on }
+    assert_raise(Whois::PropertyNotSupported) { @klass.new(load_part('/available.txt')).expires_on }
+  end
+
+end
