4ceb7a8a6a58e96938fdbf84573ed9182bca6273
Simone Carletti <weppos@weppos.net>
2010-01-16 22:21:59 +0100

    Added simple .pro TLD parser (whois.registrypro.pro).

---
+++ lib/whois/answer/parser/whois.registrypro.pro.rb
@@ -0,0 +1,77 @@
+#
+# = Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+#
+# Category::    Net
+# Package::     Whois
+# Author::      Simone Carletti <weppos@weppos.net>
+# License::     MIT License
+#
+#--
+#
+#++
+
+
+require 'whois/answer/parser/base'
+
+
+module Whois
+  class Answer
+    class Parser
+
+      #
+      # = whois.registrypro.pro parser
+      #
+      # Parser for the whois.registrypro.pro server.
+      #
+      # NOTE: This parser is just a stub and provides only a few basic methods
+      # to check for domain availability and get domain status.
+      # Please consider to contribute implementing missing methods.
+      # See WhoisNicIt parser for an explanation of all available methods
+      # and examples.
+      #
+      class WhoisRegistryproPro < Base
+
+        property_supported :status do
+          @status ||= content.to_s.scan(/Status:(.*?)\r\n/).flatten
+        end
+
+        property_supported :available? do
+          @available ||= !!(content.to_s =~ /No match/)
+        end
+
+        property_supported :registered? do
+          !available?
+        end
+
+
+        property_supported :created_on do
+          @created_on ||= if content.to_s =~ /Created On:(.*)\r\n/
+            Time.parse($1)
+          end
+        end
+
+        property_supported :updated_on do
+          @updated_on ||= if content.to_s =~ /Last Updated On:(.*)\r\n/
+            Time.parse($1)
+          end
+        end
+
+        property_supported :expires_on do
+          @expires_on ||= if content.to_s =~ /Expiration Date:(.*)\r\n/
+            Time.parse($1)
+          end
+        end
+
+
+        property_supported :nameservers do
+          @nameservers ||= content.to_s.scan(/Name Server:(.*?)\r\n/).flatten.map { |value| value.downcase }
+        end
+
+      end
+      
+    end
+  end
+end  
---
+++ test/answer/parser/whois.registrypro.pro_test.rb
@@ -0,0 +1,84 @@
+require 'test_helper'
+require 'whois/answer/parser/whois.registrypro.pro.rb'
+
+class AnswerParserWhoisRegistryproProTest < Whois::Answer::Parser::TestCase
+
+  def setup
+    @klass  = Whois::Answer::Parser::WhoisRegistryproPro
+    @host   = "whois.registrypro.pro"
+  end
+
+
+  def test_status
+    parser    = @klass.new(load_part('/registered.txt'))
+    expected  = %w( serverDeleteProhibited )
+    assert_equal  expected, parser.status
+    assert_equal  expected, parser.instance_variable_get(:"@status")
+
+    parser    = @klass.new(load_part('/available.txt'))
+    expected  = %w()
+    assert_equal  expected, parser.status
+    assert_equal  expected, parser.instance_variable_get(:"@status")
+  end
+
+  def test_available?
+    assert !@klass.new(load_part('/registered.txt')).available?
+    assert  @klass.new(load_part('/available.txt')).available?
+  end
+
+  def test_registered?
+    assert  @klass.new(load_part('/registered.txt')).registered?
+    assert !@klass.new(load_part('/available.txt')).registered?
+  end
+
+
+  def test_created_on
+    parser    = @klass.new(load_part('/registered.txt'))
+    expected  = Time.parse("2004-08-18 06:20:14 UTC")
+    assert_equal  expected, parser.created_on
+    assert_equal  expected, parser.instance_variable_get(:"@created_on")
+
+    parser    = @klass.new(load_part('/available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.created_on
+    assert_equal  expected, parser.instance_variable_get(:"@created_on")
+  end
+
+  def test_updated_on
+    parser    = @klass.new(load_part('/registered.txt'))
+    expected  = Time.parse("2009-01-20 16:51:04 UTC")
+    assert_equal  expected, parser.updated_on
+    assert_equal  expected, parser.instance_variable_get(:"@updated_on")
+
+    parser    = @klass.new(load_part('/available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.updated_on
+    assert_equal  expected, parser.instance_variable_get(:"@updated_on")
+  end
+
+  def test_expires_on
+    parser    = @klass.new(load_part('/registered.txt'))
+    expected  = Time.parse("2017-01-26 06:00:00 UTC")
+    assert_equal  expected, parser.expires_on
+    assert_equal  expected, parser.instance_variable_get(:"@expires_on")
+
+    parser    = @klass.new(load_part('/available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.expires_on
+    assert_equal  expected, parser.instance_variable_get(:"@expires_on")
+  end
+
+
+  def test_nameservers
+    parser    = @klass.new(load_part('/registered.txt'))
+    expected  = %w( a.gtld.pro b.gtld.pro c.gtld.pro d.gtld.pro )
+    assert_equal  expected, parser.nameservers
+    assert_equal  expected, parser.instance_variable_get(:"@nameservers")
+
+    parser    = @klass.new(load_part('/available.txt'))
+    expected  = %w()
+    assert_equal  expected, parser.nameservers
+    assert_equal  expected, parser.instance_variable_get(:"@nameservers")
+  end
+
+end
