0818be34c78e4e2ab04602078c388c5b830f5166
Simone Carletti <weppos@weppos.net>
2010-03-29 16:19:39 +0200

    Added simple .si TLD parser (whois.arnes.si)

---
+++ lib/whois/answer/parser/whois.arnes.si.rb
@@ -0,0 +1,75 @@
+#
+# = Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+#
+# Category::    Net
+# Package::     Whois
+# Author::      Simone Carletti <weppos@weppos.net>
+# License::     MIT License
+#
+#--
+#
+#++
+
+
+require 'whois/answer/parser/base'
+
+
+module Whois
+  class Answer
+    class Parser
+
+      #
+      # = whois.arnes.si parser
+      #
+      # Parser for the whois.arnes.si server.
+      #
+      # NOTE: This parser is just a stub and provides only a few basic methods
+      # to check for domain availability and get domain status.
+      # Please consider to contribute implementing missing methods.
+      # See WhoisNicIt parser for an explanation of all available methods
+      # and examples.
+      #
+      class WhoisArnesSi < Base
+
+        property_supported :status do
+          @status ||= if content_for_scanner =~ /status:\s+(.*)\n/
+            $1.downcase.to_sym
+          end
+        end
+
+        property_supported :available? do
+          @available ||= !!(content_for_scanner =~ /% No entries found/)
+        end
+
+        property_supported :registered? do
+          !available?
+        end
+
+
+        property_supported :created_on do
+          @created_on ||= if content_for_scanner =~ /created:\s+(.*)\n/
+            Time.parse($1)
+          end
+        end
+
+        property_not_supported :updated_on
+
+        property_supported :expires_on do
+          @expires_on ||= if content_for_scanner =~ /expire:\s+(.*)\n/
+            Time.parse($1)
+          end
+        end
+
+
+        property_supported :nameservers do
+          @nameservers ||= content_for_scanner.scan(/nameserver:\s+(.+)\n/).flatten
+        end
+
+      end
+
+    end
+  end
+end
---
+++ test/answer/parser/whois.arnes.si_test.rb
@@ -0,0 +1,72 @@
+require 'test_helper'
+require 'whois/answer/parser/whois.arnes.si.rb'
+
+class AnswerParserWhoisArnesSiTest < Whois::Answer::Parser::TestCase
+
+  def setup
+    @klass  = Whois::Answer::Parser::WhoisArnesSi
+    @host   = "whois.arnes.si"
+  end
+
+
+  def test_status
+    assert_equal  nil,
+                  @klass.new(load_part('/available.txt')).status
+    assert_equal  :ok,
+                  @klass.new(load_part('/registered.txt')).status
+  end
+
+  def test_available?
+    assert !@klass.new(load_part('/registered.txt')).available?
+    assert  @klass.new(load_part('/available.txt')).available?
+  end
+
+  def test_registered?
+    assert  @klass.new(load_part('/registered.txt')).registered?
+    assert !@klass.new(load_part('/available.txt')).registered?
+  end
+
+
+  def test_created_on
+    parser    = @klass.new(load_part('/registered.txt'))
+    expected  = Time.parse("2005-04-04")
+    assert_equal  expected, parser.created_on
+    assert_equal  expected, parser.instance_eval { @created_on }
+
+    parser    = @klass.new(load_part('/available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.created_on
+    assert_equal  expected, parser.instance_eval { @created_on }
+  end
+
+  def test_updated_on
+    assert_raise(Whois::PropertyNotSupported) { @klass.new(load_part('/registered.txt')).updated_on }
+    assert_raise(Whois::PropertyNotSupported) { @klass.new(load_part('/available.txt')).updated_on }
+  end
+
+  def test_expires_on
+    parser    = @klass.new(load_part('/registered.txt'))
+    expected  = Time.parse("2010-07-19")
+    assert_equal  expected, parser.expires_on
+    assert_equal  expected, parser.instance_eval { @expires_on }
+
+    parser    = @klass.new(load_part('/available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.expires_on
+    assert_equal  expected, parser.instance_eval { @expires_on }
+  end
+
+
+  def test_nameservers
+    parser    = @klass.new(load_part('/registered.txt'))
+    expected  = %w( ns1.google.com ns2.google.com ns3.google.com ns4.google.com )
+    assert_equal  expected, parser.nameservers
+    assert_equal  expected, parser.instance_eval { @nameservers }
+
+    parser    = @klass.new(load_part('/available.txt'))
+    expected  = %w()
+    assert_equal  expected, parser.nameservers
+    assert_equal  expected, parser.instance_eval { @nameservers }
+  end
+
+end
