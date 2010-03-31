40cd50bbd583172805dcdb2b4e5cf5aa39d42428
Simone Carletti <weppos@weppos.net>
2010-03-31 11:28:52 +0200

    Added simple .kr TLD parser (whois.nic.or.kr)

---
+++ test/answer/parser/whois.nic.or.kr_test.rb
@@ -0,0 +1,64 @@
+require 'test_helper'
+require 'whois/answer/parser/whois.nic.or.kr.rb'
+
+class AnswerParserWhoisNicOrKzTest < Whois::Answer::Parser::TestCase
+
+  def setup
+    @klass  = Whois::Answer::Parser::WhoisNicOrKz
+    @host   = "whois.nic.or.kr"
+  end
+
+
+  def test_status
+    assert_equal  :registered,
+                  @klass.new(load_part('/registered.txt')).status
+    assert_equal  :available,
+                  @klass.new(load_part('/available.txt')).status
+  end
+
+  def test_available?
+    assert !@klass.new(load_part('/registered.txt')).available?
+    assert  @klass.new(load_part('/available.txt')).available?
+  end
+
+  def test_registered?
+    assert  @klass.new(load_part('/registered.txt')).registered?
+    assert !@klass.new(load_part('/available.txt')).registered?
+  end
+
+
+  def test_created_on
+    assert_equal  Time.parse("2007-03-02"),
+                  @klass.new(load_part('/registered.txt')).created_on
+    assert_equal  nil,
+                  @klass.new(load_part('/available.txt')).created_on
+  end
+
+  def test_updated_on
+    assert_equal  Time.parse("2007-10-06"),
+                  @klass.new(load_part('/registered.txt')).updated_on
+    assert_equal  nil,
+                  @klass.new(load_part('/available.txt')).updated_on
+  end
+
+  def test_expires_on
+    assert_equal  Time.parse("2011-03-02"),
+                  @klass.new(load_part('/registered.txt')).expires_on
+    assert_equal  nil,
+                  @klass.new(load_part('/available.txt')).expires_on
+  end
+
+
+  def test_nameservers
+    parser    = @klass.new(load_part('/registered.txt'))
+    expected  = %w( ns1.google.com ns2.google.com )
+    assert_equal  expected, parser.nameservers
+    assert_equal  expected, parser.instance_eval { @nameservers }
+
+    parser    = @klass.new(load_part('/available.txt'))
+    expected  = %w()
+    assert_equal  expected, parser.nameservers
+    assert_equal  expected, parser.instance_eval { @nameservers }
+  end
+
+end
