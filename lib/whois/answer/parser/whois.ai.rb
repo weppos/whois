6963dcf2e3f5017a97ed601d54bbbdc0557758ef
Simone Carletti <weppos@weppos.net>
2010-04-18 15:18:26 +0200

    Added simple .ai TLD parser (whois.ai).

---
+++ lib/whois/answer/parser/whois.ai.rb
@@ -0,0 +1,73 @@
+#
+# = Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+#
+# Category::    Net
+# Package::     Whois
+# Author::      Simone Carletti <weppos@weppos.net>
+# License::     MIT License
+#
+#--
+#
+#++
+
+
+require 'whois/answer/parser/base'
+
+
+module Whois
+  class Answer
+    class Parser
+
+      #
+      # = whois.ai parser
+      #
+      # Parser for the whois.ai server.
+      #
+      # NOTE: This parser is just a stub and provides only a few basic methods
+      # to check for domain availability and get domain status.
+      # Please consider to contribute implementing missing methods.
+      # See WhoisNicIt parser for an explanation of all available methods
+      # and examples.
+      #
+      class WhoisAi < Base
+
+        property_supported :status do
+          if available?
+            :available
+          else
+            :registered
+          end
+        end
+
+        property_supported :available? do
+          @available ||= !!(content_for_scanner =~ /Domain (.+?) not registred/)
+        end
+
+        property_supported :registered? do
+          !available?
+        end
+
+
+        property_not_supported :created_on
+
+        property_not_supported :updated_on
+
+        property_not_supported :expires_on
+
+
+        property_supported :nameservers do
+          @nameservers ||= if content_for_scanner =~ /Nameservers\n((.+\n)+)\n/
+            $1.split("\n").select { |e| e =~ /Server Hostname/ }.map { |value| value.split(":").last.strip }
+          else
+            []
+          end
+        end
+
+      end
+
+    end
+  end
+end
