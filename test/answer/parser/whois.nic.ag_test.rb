675ce43c01d1a9f426135513bf76127fc395632d
Simone Carletti <weppos@weppos.net>
2010-04-18 15:05:52 +0200

    Added simple .ag TLD parser (whois.nic.ag).

---
+++ test/answer/parser/whois.nic.ag_test.rb
@@ -0,0 +1,79 @@
+require 'test_helper'
+require 'whois/answer/parser/whois.nic.ag.rb'
+
+class AnswerParserWhoisNicAgTest < Whois::Answer::Parser::TestCase
+
+  def setup
+    @klass  = Whois::Answer::Parser::WhoisNicAg
+    @host   = "whois.nic.ag"
+  end
+
+
+  def test_status
+    assert_equal  ["CLIENT DELETE PROHIBITED", "CLIENT TRANSFER PROHIBITED", "CLIENT UPDATE PROHIBITED"],
+                  @klass.new(load_part('/registered.txt')).status
+    assert_equal  [],
+                  @klass.new(load_part('/available.txt')).status
+  end
+
+  def test_available?
+    assert !@klass.new(load_part('/registered.txt')).available?
+    assert  @klass.new(load_part('/available.txt')).available?
+  end
+
+  def test_registered?
+    assert  @klass.new(load_part('/registered.txt')).registered?
+    assert !@klass.new(load_part('/available.txt')).registered?
+  end
+
+
+  def test_created_on
+    parser    = @klass.new(load_part('/registered.txt'))
+    expected  = Time.parse("2003-01-05 14:06:59 UTC")
+    assert_equal  expected, parser.created_on
+    assert_equal  expected, parser.instance_eval { @created_on }
+
+    parser    = @klass.new(load_part('/available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.created_on
+    assert_equal  expected, parser.instance_eval { @created_on }
+  end
+
+  def test_updated_on
+    parser    = @klass.new(load_part('/registered.txt'))
+    expected  = Time.parse("2009-12-09 19:28:04 UTC")
+    assert_equal  expected, parser.updated_on
+    assert_equal  expected, parser.instance_eval { @updated_on }
+
+    parser    = @klass.new(load_part('/available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.updated_on
+    assert_equal  expected, parser.instance_eval { @updated_on }
+  end
+
+  def test_expires_on
+    parser    = @klass.new(load_part('/registered.txt'))
+    expected  = Time.parse("2011-01-05 14:06:59 UTC")
+    assert_equal  expected, parser.expires_on
+    assert_equal  expected, parser.instance_eval { @expires_on }
+
+    parser    = @klass.new(load_part('/available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.expires_on
+    assert_equal  expected, parser.instance_eval { @expires_on }
+  end
+
+
+  def test_nameservers
+    parser    = @klass.new(load_part('/registered.txt'))
+    expected  = %w( ns1.google.com ns2.google.com ns3.google.com ns4.google.com )
+    assert_equal  expected, parser.nameservers
+    assert_equal  expected, parser.instance_eval { @nameservers }
+
+    parser    = @klass.new(load_part('/available.txt'))
+    expected  = %w()
+    assert_equal  expected, parser.nameservers
+    assert_equal  expected, parser.instance_eval { @nameservers }
+  end
+
+end
