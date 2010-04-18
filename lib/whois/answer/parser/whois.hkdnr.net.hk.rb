57e64d96d5542aadbb7ed0e25b58d4fa7d23a442
Simone Carletti <weppos@weppos.net>
2010-04-18 15:36:09 +0200

    Added simple .hk TLD parser (whois.hkdnr.net.hk).

---
+++ lib/whois/answer/parser/whois.hkdnr.net.hk.rb
@@ -0,0 +1,81 @@
+#
+# = Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+#
+# Category::    Net
+# Package::     Whois
+# Author::      Simone Carletti <weppos@weppos.net>
+# License::     MIT License
+#
+#--
+#
+#++
+
+
+require 'whois/answer/parser/base'
+
+
+module Whois
+  class Answer
+    class Parser
+
+      #
+      # = whois.hkdnr.net.hk parser
+      #
+      # Parser for the whois.hkdnr.net.hk server.
+      #
+      # NOTE: This parser is just a stub and provides only a few basic methods
+      # to check for domain availability and get domain status.
+      # Please consider to contribute implementing missing methods.
+      # See WhoisNicIt parser for an explanation of all available methods
+      # and examples.
+      #
+      class WhoisHkdnrNetHk < Base
+
+        property_supported :status do
+          @status ||= if available?
+            :available
+          else
+            :registered
+          end
+        end
+
+        property_supported :available? do
+          @available ||= (content_for_scanner.strip == "Domain Not Found")
+        end
+
+        property_supported :registered? do
+          !available?
+        end
+
+
+        property_supported :created_on do
+          @created_on ||= if content_for_scanner =~ /Domain Name Commencement Date:\s(.*?)\n/
+            Time.parse($1)
+          end
+        end
+
+        property_not_supported :updated_on
+
+        property_supported :expires_on do
+          @expires_on ||= if content_for_scanner =~ /Expiry Date:\s(.*?)\n/
+            Time.parse($1)
+          end
+        end
+
+
+        property_supported :nameservers do
+          @nameservers ||= if content_for_scanner =~ /Name Servers Information:\n\n((.+\n)+)\n/
+            $1.split("\n").map { |value| value.strip.downcase }
+          else
+            []
+          end
+        end
+
+      end
+
+    end
+  end
+end
