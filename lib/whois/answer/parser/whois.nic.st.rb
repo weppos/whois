ce844be0c534f821860e28b0226d0e200aaf010e
Simone Carletti <weppos@weppos.net>
2010-05-03 23:02:25 +0200

    Added simple .st TLD parser (whois.nic.st).

---
+++ lib/whois/answer/parser/whois.nic.st.rb
@@ -0,0 +1,81 @@
+#
+# = Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+#
+# Category::    Net
+# Package::     Whois
+# Author::      Simone Carletti <weppos@weppos.net>
+# License::     MIT License
+#
+#--
+#
+#++
+
+
+require 'whois/answer/parser/base'
+
+
+module Whois
+  class Answer
+    class Parser
+
+      #
+      # = whois.nic.st parser
+      #
+      # Parser for the whois.nic.st server.
+      #
+      # NOTE: This parser is just a stub and provides only a few basic methods
+      # to check for domain availability and get domain status.
+      # Please consider to contribute implementing missing methods.
+      # See WhoisNicIt parser for an explanation of all available methods
+      # and examples.
+      #
+      class WhoisNicSt < Base
+
+        property_supported :status do
+          @status ||= if available?
+            :available
+          else
+            :registered
+          end
+        end
+
+        property_supported :available? do
+          @available ||= !!(content_for_scanner =~ /No entries found for domain/)
+        end
+
+        property_supported :registered? do
+          !available?
+        end
+
+
+        property_supported :created_on do
+          @created_on ||= if content_for_scanner =~ /\s+Creation Date:\s+(.*)\n/
+            Time.parse($1)
+          end
+        end
+
+        property_supported :updated_on do
+          @updated_on ||= if content_for_scanner =~ /\s+Updated Date:\s+(.*)\n/
+            Time.parse($1)
+          end
+        end
+
+        property_not_supported :expires_on
+
+
+        property_supported :nameservers do
+          @nameservers ||= if content_for_scanner =~ /Name Servers:\n((.+\n)+)\n?/
+            $1.split("\n").map(&:strip)
+          else
+            []
+          end
+        end
+
+      end
+
+    end
+  end
+end
