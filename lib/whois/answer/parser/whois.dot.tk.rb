20468c80834c679bf538d4f3fc354af68ffc9817
Simone Carletti <weppos@weppos.net>
2010-05-07 11:19:49 +0200

    Added simple .tk TLD parser (whois.dot.tk).

---
+++ lib/whois/answer/parser/whois.dot.tk.rb
@@ -0,0 +1,81 @@
+#
+# = Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+#
+# Category::    Net
+# Package::     Whois
+# Author::      Simone Carletti <weppos@weppos.net>
+# License::     MIT License
+#
+#--
+#
+#++
+
+
+require 'whois/answer/parser/base'
+
+
+module Whois
+  class Answer
+    class Parser
+
+      #
+      # = whois.dot.tk parser
+      #
+      # Parser for the whois.dot.tk server.
+      #
+      # NOTE: This parser is just a stub and provides only a few basic methods
+      # to check for domain availability and get domain status.
+      # Please consider to contribute implementing missing methods.
+      # See WhoisNicIt parser for an explanation of all available methods
+      # and examples.
+      #
+      class WhoisDotTk < Base
+
+        property_supported :status do
+          @status ||= if available?
+            :available
+          else
+            :registered
+          end
+        end
+
+        property_supported :available? do
+          @available ||= !!(content_for_scanner =~ /domain name not known/)
+        end
+
+        property_supported :registered? do
+          !available?
+        end
+
+
+        property_supported :created_on do
+          @created_on ||= if content_for_scanner =~ /Domain registered:\s+(.*)\n/
+            Time.parse($1)
+          end
+        end
+
+        property_not_supported :updated_on
+
+        property_supported :expires_on do
+          @expires_on ||= if content_for_scanner =~ /Record will expire on:\s+(.*)\n/
+            Time.parse($1)
+          end
+        end
+
+
+        property_supported :nameservers do
+          @nameservers ||= if content_for_scanner =~ /Domain Nameservers:\n((.+\n)+)\s+\n/
+            $1.split("\n").map { |value| value.strip.downcase }
+          else
+            []
+          end
+        end
+
+      end
+
+    end
+  end
+end
