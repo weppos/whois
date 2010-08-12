37ac08dd059764949a76d544ce485eb205aa2b6d
Simone Carletti <weppos@weppos.net>
2010-08-12 17:24:42 +0200

    Release 1.3.0

--- lib/whois/version.rb
+++ lib/whois/version.rb
@@ -18,10 +18,11 @@ module Whois
 
   module Version
     MAJOR = 1
-    MINOR = 2
-    PATCH = 2
+    MINOR = 3
+    PATCH = 0
+    BUILD = nil
 
-    STRING = [MAJOR, MINOR, PATCH].compact.join(".")
+    STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join(".")
   end
 
   VERSION = Version::STRING
--- whois.gemspec
+++ whois.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |s|
   s.name = %q{whois}
-  s.version = "1.2.2"
+  s.version = "1.3.0"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Simone Carletti"]
