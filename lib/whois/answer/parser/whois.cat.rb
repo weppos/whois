31ca6bf9a44d7b1ba065394a9c2ffb2326a60eee
Simone Carletti <weppos@weppos.net>
2010-08-24 01:09:30 +0200

    Enhanced whois.cat test suite

--- lib/whois/answer/parser/whois.cat.rb
+++ lib/whois/answer/parser/whois.cat.rb
@@ -41,11 +41,11 @@ module Whois
         end
 
         property_supported :available? do
-          @available ||= !!(content_for_scanner =~ /Object (.*?) NOT FOUND/)
+          @available  ||= !!(content_for_scanner =~ /Object (.*?) NOT FOUND/)
         end
 
         property_supported :registered? do
-          !available?
+          @registered ||= !available?
         end
 
 
