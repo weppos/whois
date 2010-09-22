afd3cd7df2cfaa298d54c275e77c6a2172fd9f13
Simone Carletti <weppos@weppos.net>
2010-09-22 17:31:33 +0200

    whois_test.rb => whois_rspec.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,17 @@
 require "rspec"
 require "whois"
 
+module Helpers
+  def with_definitions(&block)
+    @_definitions = Whois::Server.definitions
+    Whois::Server.send :class_variable_set, :@@definitions, { :tld => [], :ipv4 =>[], :ipv6 => [] }
+    yield
+  ensure
+    Whois::Server.send :class_variable_set, :@@definitions, @_definitions
+  end
+end
+
 RSpec.configure do |config|
-  config.mock_with :rr
+  config.mock_with :mocha
+  config.include Helpers
 end
