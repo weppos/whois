b5728ba6aacd7892512f5947c20fc6972d284258
Simone Carletti <weppos@weppos.net>
2010-10-13 00:22:49 +0200

    Added simple .ir TLD parser (whois.nic.ir).

---
+++ lib/whois/answer/parser/whois.nic.ir.rb
@@ -0,0 +1,73 @@
+#
+# = Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+#
+# Category::    Net
+# Package::     Whois
+# Author::      Simone Carletti <weppos@weppos.net>
+# License::     MIT License
+#
+#--
+#
+#++
+
+
+require 'whois/answer/parser/base'
+
+
+module Whois
+  class Answer
+    class Parser
+
+      #
+      # = whois.nic.ir parser
+      #
+      # Parser for the whois.nic.ir server.
+      #
+      # NOTE: This parser is just a stub and provides only a few basic methods
+      # to check for domain availability and get domain status.
+      # Please consider to contribute implementing missing methods.
+      # See WhoisNicIt parser for an explanation of all available methods
+      # and examples.
+      #
+      class WhoisNicIr < Base
+
+        property_supported :status do
+          @status ||= if available?
+            :available
+          else
+            :registered
+          end
+        end
+
+        property_supported :available? do
+          @available  ||= !!(content_for_scanner =~ /%ERROR:101: no entries found/)
+        end
+
+        property_supported :registered? do
+          @registered ||= !available?
+        end
+
+
+        property_not_supported :created_on
+
+        property_supported :updated_on do
+          @updated_on ||= if content_for_scanner =~ /last-updated:\s+(.*)\n/
+            Time.parse($1)
+          end
+        end
+
+        property_not_supported :expires_on
+
+
+        property_supported :nameservers do
+          @nameservers ||= content_for_scanner.scan(/nserver:\s+(.+)\n/).flatten
+        end
+
+      end
+
+    end
+  end
+end
