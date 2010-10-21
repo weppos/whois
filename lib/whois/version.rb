9d2f8f7e0e01ae594466e36e82b52f8f8e649338
Simone Carletti <weppos@weppos.net>
2010-10-21 21:21:40 +0200

    Change to development release.

--- lib/whois/version.rb
+++ lib/whois/version.rb
@@ -18,8 +18,8 @@ module Whois
 
   module Version
     MAJOR = 1
-    MINOR = 3
-    PATCH = 10
+    MINOR = 5
+    PATCH = "dev"
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join(".")
