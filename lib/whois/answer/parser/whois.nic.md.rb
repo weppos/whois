6a7ba24af4cb52daa88e8555fa8ddf498e1129b2
Simone Carletti <weppos@weppos.net>
2010-10-24 00:42:48 +0200

    Added simple .md TLD parser (whois.nic.md).

---
+++ lib/whois/answer/parser/whois.nic.md.rb
@@ -0,0 +1,77 @@
+#
+# = Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+#
+# Category::    Net
+# Package::     Whois
+# Author::      Simone Carletti <weppos@weppos.net>
+# License::     MIT License
+#
+#--
+#
+#++
+
+
+require 'whois/answer/parser/base'
+
+
+module Whois
+  class Answer
+    class Parser
+
+      #
+      # = whois.nic.md parser
+      #
+      # Parser for the whois.nic.md server.
+      #
+      # NOTE: This parser is just a stub and provides only a few basic methods
+      # to check for domain availability and get domain status.
+      # Please consider to contribute implementing missing methods.
+      # See WhoisNicIt parser for an explanation of all available methods
+      # and examples.
+      #
+      class WhoisNicMd < Base
+
+        property_supported :status do
+          @status ||= if available?
+            :available
+          else
+            :registered
+          end
+        end
+
+        property_supported :available? do
+          @available  ||= !!(content_for_scanner.strip == "No match for")
+        end
+
+        property_supported :registered? do
+          @registered ||= !available?
+        end
+
+
+        property_supported :created_on do
+          @created_on ||= if content_for_scanner =~ /Created:\s(.+?)\n/
+            Time.parse($1)
+          end
+        end
+
+        property_not_supported :updated_on
+
+        property_supported :expires_on do
+          @expires_on ||= if content_for_scanner =~ /Expiration date:\s+(.+?)\n/
+            Time.parse($1)
+          end
+        end
+
+
+        property_supported :nameservers do
+          @nameservers ||= content_for_scanner.scan(/Name server:\s(.+?)\n/).flatten.map { |value| value.split(" ").first }
+        end
+
+      end
+
+    end
+  end
+end
