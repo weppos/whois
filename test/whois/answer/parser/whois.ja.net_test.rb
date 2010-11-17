469857fc46cad247a91708d16295926efbe20282
Simone Carletti <weppos@weppos.net>
2010-11-17 21:04:32 +0100

    Added simple .ac.uk, .gov.uk TLD parser (whois.ja.net).

---
+++ test/whois/answer/parser/whois.ja.net_test.rb
@@ -0,0 +1,191 @@
+require 'test_helper'
+require 'whois/answer/parser/whois.ja.net'
+
+class AnswerParserWhoisJaNetTest < Whois::Answer::Parser::TestCase
+
+  def setup
+    @klass  = Whois::Answer::Parser::WhoisJaNet
+    @host   = "whois.ja.net"
+  end
+
+end
+
+class AnswerParserWhoisJaNetAcUkTest < AnswerParserWhoisJaNetTest
+
+  def test_status
+    parser    = @klass.new(load_part('/ac.uk/registered.txt'))
+    expected  = :registered
+    assert_equal  expected, parser.status
+    assert_equal  expected, parser.instance_eval { @status }
+
+    parser    = @klass.new(load_part('/ac.uk/available.txt'))
+    expected  = :available
+    assert_equal  expected, parser.status
+    assert_equal  expected, parser.instance_eval { @status }
+  end
+
+  def test_available?
+    parser    = @klass.new(load_part('/ac.uk/registered.txt'))
+    expected  = false
+    assert_equal  expected, parser.available?
+    assert_equal  expected, parser.instance_eval { @available }
+
+    parser    = @klass.new(load_part('/ac.uk/available.txt'))
+    expected  = true
+    assert_equal  expected, parser.available?
+    assert_equal  expected, parser.instance_eval { @available }
+  end
+
+  def test_registered?
+    parser    = @klass.new(load_part('/ac.uk/registered.txt'))
+    expected  = true
+    assert_equal  expected, parser.registered?
+    assert_equal  expected, parser.instance_eval { @registered }
+
+    parser    = @klass.new(load_part('/ac.uk/available.txt'))
+    expected  = false
+    assert_equal  expected, parser.registered?
+    assert_equal  expected, parser.instance_eval { @registered }
+  end
+
+
+  def test_created_on
+    parser    = @klass.new(load_part('/ac.uk/registered.txt'))
+    expected  = Time.parse("2003-11-07")
+    assert_equal  expected, parser.created_on
+    assert_equal  expected, parser.instance_eval { @created_on }
+
+    parser    = @klass.new(load_part('/ac.uk/available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.created_on
+    assert_equal  expected, parser.instance_eval { @created_on }
+  end
+
+  def test_updated_on
+    parser    = @klass.new(load_part('/ac.uk/registered.txt'))
+    expected  = Time.parse("2010-05-16")
+    assert_equal  expected, parser.updated_on
+    assert_equal  expected, parser.instance_eval { @updated_on }
+
+    parser    = @klass.new(load_part('/ac.uk/available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.updated_on
+    assert_equal  expected, parser.instance_eval { @updated_on }
+  end
+
+  def test_expires_on
+    parser    = @klass.new(load_part('/ac.uk/registered.txt'))
+    expected  = Time.parse("2012-06-16")
+    assert_equal  expected, parser.expires_on
+    assert_equal  expected, parser.instance_eval { @expires_on }
+
+    parser    = @klass.new(load_part('/ac.uk/available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.expires_on
+    assert_equal  expected, parser.instance_eval { @expires_on }
+  end
+
+
+  def test_nameservers
+    parser    = @klass.new(load_part('/ac.uk/registered.txt'))
+    expected  = %w( agate.lut.ac.uk bgate.lut.ac.uk cgate.lut.ac.uk ns3.ja.net )
+    assert_equal  expected, parser.nameservers
+    assert_equal  expected, parser.instance_eval { @nameservers }
+
+    parser    = @klass.new(load_part('/ac.uk/available.txt'))
+    expected  = %w()
+    assert_equal  expected, parser.nameservers
+    assert_equal  expected, parser.instance_eval { @nameservers }
+  end
+
+end
+
+class AnswerParserWhoisJaNetGovUkTest < AnswerParserWhoisJaNetTest
+
+  def test_status
+    parser    = @klass.new(load_part('/gov.uk/registered.txt'))
+    expected  = :registered
+    assert_equal  expected, parser.status
+    assert_equal  expected, parser.instance_eval { @status }
+
+    parser    = @klass.new(load_part('/gov.uk/available.txt'))
+    expected  = :available
+    assert_equal  expected, parser.status
+    assert_equal  expected, parser.instance_eval { @status }
+  end
+
+  def test_available?
+    parser    = @klass.new(load_part('/gov.uk/registered.txt'))
+    expected  = false
+    assert_equal  expected, parser.available?
+    assert_equal  expected, parser.instance_eval { @available }
+
+    parser    = @klass.new(load_part('/gov.uk/available.txt'))
+    expected  = true
+    assert_equal  expected, parser.available?
+    assert_equal  expected, parser.instance_eval { @available }
+  end
+
+  def test_registered?
+    parser    = @klass.new(load_part('/gov.uk/registered.txt'))
+    expected  = true
+    assert_equal  expected, parser.registered?
+    assert_equal  expected, parser.instance_eval { @registered }
+
+    parser    = @klass.new(load_part('/gov.uk/available.txt'))
+    expected  = false
+    assert_equal  expected, parser.registered?
+    assert_equal  expected, parser.instance_eval { @registered }
+  end
+
+
+  def test_created_on
+    parser    = @klass.new(load_part('/gov.uk/registered.txt'))
+    expected  = Time.parse("2003-09-17")
+    assert_equal  expected, parser.created_on
+    assert_equal  expected, parser.instance_eval { @created_on }
+
+    parser    = @klass.new(load_part('/gov.uk/available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.created_on
+    assert_equal  expected, parser.instance_eval { @created_on }
+  end
+
+  def test_updated_on
+    parser    = @klass.new(load_part('/gov.uk/registered.txt'))
+    expected  = Time.parse("2010-01-13")
+    assert_equal  expected, parser.updated_on
+    assert_equal  expected, parser.instance_eval { @updated_on }
+
+    parser    = @klass.new(load_part('/gov.uk/available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.updated_on
+    assert_equal  expected, parser.instance_eval { @updated_on }
+  end
+
+  def test_expires_on
+    parser    = @klass.new(load_part('/gov.uk/registered.txt'))
+    expected  = Time.parse("2011-03-30")
+    assert_equal  expected, parser.expires_on
+    assert_equal  expected, parser.instance_eval { @expires_on }
+
+    parser    = @klass.new(load_part('/gov.uk/available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.expires_on
+    assert_equal  expected, parser.instance_eval { @expires_on }
+  end
+
+
+  def test_nameservers
+    parser    = @klass.new(load_part('/gov.uk/registered.txt'))
+    expected  = %w( eur5.akam.net eur6.akam.net ns1-173.akam.net ns1-31.akam.net usc4.akam.net use10.akam.net usw2.akam.net usw4.akam.net )
+    assert_equal  expected, parser.nameservers
+    assert_equal  expected, parser.instance_eval { @nameservers }
+
+    parser    = @klass.new(load_part('/gov.uk/available.txt'))
+    expected  = %w()
+    assert_equal  expected, parser.nameservers
+    assert_equal  expected, parser.instance_eval { @nameservers }
+  end
+
+end
