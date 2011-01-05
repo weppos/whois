0da8f2bbc7b749761f4e5275148c626024d9c560
Simone Carletti <weppos@weppos.net>
2011-01-05 22:12:42 +0100

    Partially reverted commit 07bffc27a5c44b4cf0ef1b2ee45bee65df6454fe.
    whois.eenet.ee and whois.tld.ee are so different that an alias doesn't make sense,
    thought I originally suggested it.
    https://github.com/weppos/whois/pull/62

--- lib/whois/answer/parser/whois.eenet.ee.rb
+++ lib/whois/answer/parser/whois.eenet.ee.rb
@@ -14,19 +14,67 @@
 #++
 
 
-require 'whois/answer/parser/whois.tld.ee'
+require 'whois/answer/parser/base'
+
 
 module Whois
   class Answer
     class Parser
 
-      #
-      # = whois.eenet.ee parser
-      #
       # Parser for the whois.eenet.ee server.
-      # Alias for whois.tld.ee parser.
       #
-      WhoisEenetEe = WhoisTldEe
+      # NOTE: This parser is just a stub and provides only a few basic methods
+      # to check for domain availability and get domain status.
+      # Please consider to contribute implementing missing methods.
+      # See WhoisNicIt parser for an explanation of all available methods
+      # and examples.
+      #
+      # @deprecated The official .ee TLD host is now whois.tld.ee.
+      #             This parser only exists for BC.
+      #
+      class WhoisEenetEe < Base
+
+        property_supported :status do
+          @status ||= if available?
+            :available
+          else
+            :registered
+          end
+        end
+
+        property_supported :available? do
+          @available ||= !!(content_for_scanner =~ /^NOT FOUND:/)
+        end
+
+        property_supported :registered? do
+          @registered ||= !available?
+        end
+
+
+        property_supported :created_on do
+          @created_on ||= if content_for_scanner =~ /Record created on (.*)\n/
+            Time.parse($1)
+          end
+        end
+
+        property_supported :updated_on do
+          @updated_on ||= if content_for_scanner =~ /Record changed on (.*)\n/
+            Time.parse($1)
+          end
+        end
+
+        property_not_supported :expires_on
+
+
+        property_supported :nameservers do
+          @nameservers ||= if content_for_scanner =~ /Domain servers:\n((.+\n)+)\n/
+            $1.split("\n").map { |value| value.strip.chomp(".") }
+          else
+            []
+          end
+        end
+
+      end
 
     end
   end
--- test/whois/answer/parser/whois.eenet.ee_test.rb
+++ test/whois/answer/parser/whois.eenet.ee_test.rb
@@ -1,12 +1,91 @@
 require 'test_helper'
 require 'whois/answer/parser/whois.eenet.ee'
-require 'whois/answer/parser/whois.tld.ee_test'
 
-class AnswerParserWhoisEenetEeTest < AnswerParserWhoisTldEeTest
+class AnswerParserWhoisEenetEeTest < Whois::Answer::Parser::TestCase
 
   def setup
     @klass  = Whois::Answer::Parser::WhoisEenetEe
-    @host   = "whois.tld.ee"
+    @host   = "whois.eenet.ee"
+  end
+
+
+  def test_status
+    parser    = @klass.new(load_part('registered.txt'))
+    expected  = :registered
+    assert_equal  expected, parser.status
+    assert_equal  expected, parser.instance_eval { @status }
+
+    parser    = @klass.new(load_part('available.txt'))
+    expected  = :available
+    assert_equal  expected, parser.status
+    assert_equal  expected, parser.instance_eval { @status }
+  end
+
+  def test_available?
+    parser    = @klass.new(load_part('registered.txt'))
+    expected  = false
+    assert_equal  expected, parser.available?
+    assert_equal  expected, parser.instance_eval { @available }
+
+    parser    = @klass.new(load_part('available.txt'))
+    expected  = true
+    assert_equal  expected, parser.available?
+    assert_equal  expected, parser.instance_eval { @available }
+  end
+
+  def test_registered?
+    parser    = @klass.new(load_part('registered.txt'))
+    expected  = true
+    assert_equal  expected, parser.registered?
+    assert_equal  expected, parser.instance_eval { @registered }
+
+    parser    = @klass.new(load_part('available.txt'))
+    expected  = false
+    assert_equal  expected, parser.registered?
+    assert_equal  expected, parser.instance_eval { @registered }
+  end
+
+
+  def test_created_on
+    parser    = @klass.new(load_part('registered.txt'))
+    expected  = Time.parse("2003-04-22")
+    assert_equal  expected, parser.created_on
+    assert_equal  expected, parser.instance_eval { @created_on }
+
+    parser    = @klass.new(load_part('available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.created_on
+    assert_equal  expected, parser.instance_eval { @created_on }
+  end
+
+  def test_updated_on
+    parser    = @klass.new(load_part('registered.txt'))
+    expected  = Time.parse("2010-05-28")
+    assert_equal  expected, parser.updated_on
+    assert_equal  expected, parser.instance_eval { @updated_on }
+
+    parser    = @klass.new(load_part('available.txt'))
+    expected  = nil
+    assert_equal  expected, parser.updated_on
+    assert_equal  expected, parser.instance_eval { @updated_on }
+  end
+
+  def test_expires_on
+    assert_raise(Whois::PropertyNotSupported) { @klass.new(load_part('registered.txt')).expires_on }
+    assert_raise(Whois::PropertyNotSupported) { @klass.new(load_part('available.txt')).expires_on }
+  end
+
+
+  def test_nameservers
+    parser    = @klass.new(load_part('registered.txt'))
+    expected  = %w( ns1.google.com ns2.google.com )
+    assert_equal  expected, parser.nameservers
+    assert_equal  expected, parser.instance_eval { @nameservers }
+
+    parser    = @klass.new(load_part('available.txt'))
+    expected  = %w()
+    assert_equal  expected, parser.nameservers
+    assert_equal  expected, parser.instance_eval { @nameservers }
   end
 
 end
