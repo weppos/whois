3994ada6fcb47f8efc7d5a58d0807620833539e5
Simone Carletti <weppos@weppos.net>
2011-01-20 15:36:59 +0100

    Fixed tests invalidated by the bind address feature.

--- spec/whois/server/adapters/pir_spec.rb
+++ spec/whois/server/adapters/pir_spec.rb
@@ -13,7 +13,7 @@ describe Whois::Server::Adapters::Pir do
       it "returns the WHOIS record" do
         response = "No match for DOMAIN.TEST."
         expected = response
-        @server.expects(:ask_the_socket).with("FULL domain.test", "whois.publicinterestregistry.net", 43).returns(response)
+        @server.expects(:ask_the_socket).with("FULL domain.test", "whois.publicinterestregistry.net", 43, nil, nil).returns(response)
 
         record = @server.query("domain.test")
         record.to_s.should  == expected
@@ -27,8 +27,8 @@ describe Whois::Server::Adapters::Pir do
         referral = File.read(fixture("referrals/pir.org.txt"))
         response = "Match for DOMAIN.TEST."
         expected = referral + "\n" + response
-        @server.expects(:ask_the_socket).with("FULL domain.test", "whois.publicinterestregistry.net", 43).returns(referral)
-        @server.expects(:ask_the_socket).with("domain.test", "whois.iana.org", 43).returns(response)
+        @server.expects(:ask_the_socket).with("FULL domain.test", "whois.publicinterestregistry.net", 43, nil, nil).returns(referral)
+        @server.expects(:ask_the_socket).with("domain.test", "whois.iana.org", 43, nil, nil).returns(response)
 
         record = @server.query("domain.test")
         record.to_s.should  == expected
