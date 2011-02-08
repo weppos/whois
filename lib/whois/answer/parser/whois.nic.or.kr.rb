079e3c90a0effdb36e5cc61996bb7ac186bdab85
Simone Carletti <weppos@weppos.net>
2011-02-08 11:24:31 +0100

    Changed .KR TLD definition to whois.kr.

--- lib/whois/answer/parser/whois.nic.or.kr.rb
+++ lib/whois/answer/parser/whois.nic.or.kr.rb
@@ -14,67 +14,16 @@
 #++
 
 
-require 'whois/answer/parser/base'
+require 'whois/answer/parser/whois.kr'
 
 
 module Whois
   class Answer
     class Parser
 
-      #
-      # = whois.nic.or.kr parser
-      #
-      # Parser for the whois.nic.or.kr server.
-      #
-      # NOTE: This parser is just a stub and provides only a few basic methods
-      # to check for domain availability and get domain status.
-      # Please consider to contribute implementing missing methods.
-      # See WhoisNicIt parser for an explanation of all available methods
-      # and examples.
-      #
-      class WhoisNicOrKr < Base
-
-        property_supported :status do
-          @status ||= if available?
-            :available
-          else
-            :registered
-          end
-        end
-
-        property_supported :available? do
-          @available  ||= !!(content_for_scanner =~ /domain name is not registered/)
-        end
-
-        property_supported :registered? do
-          @registered ||= !available?
-        end
-
-
-        property_supported :created_on do
-          @created_on ||= if content_for_scanner =~ /Registered Date\s+:\s+(.*)\n/
-            Time.parse($1)
-          end
-        end
-
-        property_supported :updated_on do
-          @updated_on ||= if content_for_scanner =~ /Last updated Date\s+:\s+(.*)\n/
-            Time.parse($1)
-          end
-        end
-
-        property_supported :expires_on do
-          @expires_on ||= if content_for_scanner =~ /Expiration Date\s+:\s+(.*)\n/
-            Time.parse($1)
-          end
-        end
-
-
-        property_supported :nameservers do
-          @nameservers ||= content_for_scanner.scan(/\s+Host Name\s+:\s+(.*)\n/).flatten
-        end
-
-      end
+      # Parser for the <tt>whois.nic.or.kr</tt> server.
+      # Aliases the <tt>whois.kr</tt>.
+      WhoisNicOrKr = WhoisKr
 
     end
   end
