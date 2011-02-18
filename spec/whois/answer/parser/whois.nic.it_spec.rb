e8c447b5ee859524886afc6b9ef61f4442a23e72
Simone Carletti <weppos@weppos.net>
2011-02-18 22:31:56 +0100

    Update context

--- spec/whois/answer/parser/whois.nic.it_spec.rb
+++ spec/whois/answer/parser/whois.nic.it_spec.rb
@@ -10,42 +10,42 @@ describe Whois::Answer::Parser::WhoisNicIt do
 
   # TODO: test property patterns instead of each different type
   describe "#status" do
-    context "status registered" do
+    context "value is active" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_active.txt'))
         expected  = :registered
         parser.should support_property_and_cache(:status, expected)
       end
     end
-    context "status ok" do
+    context "value is ok" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_ok.txt'))
         expected  = :registered
         parser.should support_property_and_cache(:status, expected)
       end
     end
-    context "status ok autorenew" do
+    context "value is ok autorenew" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_ok.txt'))
         expected  = :registered
         parser.should support_property_and_cache(:status, expected)
       end
     end
-    context "status client" do
+    context "value is client" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_ok.txt'))
         expected  = :registered
         parser.should support_property_and_cache(:status, expected)
       end
     end
-    context "status pendingDelete" do
+    context "value is pendingDelete" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_pendingdelete.txt'))
         expected  = :registered
         parser.should support_property_and_cache(:status, expected)
       end
     end
-    context "status available" do
+    context "value is available" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_available.txt'))
         expected  = :available
@@ -55,42 +55,42 @@ describe Whois::Answer::Parser::WhoisNicIt do
   end
 
   describe "#available?" do
-    context "status registered" do
+    context "value is active" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_active.txt'))
         expected  = false
         parser.should support_property_and_cache(:available?, expected)
       end
     end
-    context "status ok" do
+    context "value is ok" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_ok.txt'))
         expected  = false
         parser.should support_property_and_cache(:available?, expected)
       end
     end
-    context "status ok autorenew" do
+    context "value is ok autorenew" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_ok.txt'))
         expected  = false
         parser.should support_property_and_cache(:available?, expected)
       end
     end
-    context "status client" do
+    context "value is client" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_ok.txt'))
         expected  = false
         parser.should support_property_and_cache(:available?, expected)
       end
     end
-    context "status pendingDelete" do
+    context "value is pendingDelete" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_pendingdelete.txt'))
         expected  = false
         parser.should support_property_and_cache(:available?, expected)
       end
     end
-    context "status available" do
+    context "value is available" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_available.txt'))
         expected  = true
@@ -100,42 +100,42 @@ describe Whois::Answer::Parser::WhoisNicIt do
   end
 
   describe "#registered?" do
-    context "status registered" do
+    context "value is active" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_active.txt'))
         expected  = true
         parser.should support_property_and_cache(:registered?, expected)
       end
     end
-    context "status ok" do
+    context "value is ok" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_ok.txt'))
         expected  = true
         parser.should support_property_and_cache(:registered?, expected)
       end
     end
-    context "status ok autorenew" do
+    context "value is ok autorenew" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_ok.txt'))
         expected  = true
         parser.should support_property_and_cache(:registered?, expected)
       end
     end
-    context "status client" do
+    context "value is client" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_ok.txt'))
         expected  = true
         parser.should support_property_and_cache(:registered?, expected)
       end
     end
-    context "status pendingDelete" do
+    context "value is pendingDelete" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_pendingdelete.txt'))
         expected  = true
         parser.should support_property_and_cache(:registered?, expected)
       end
     end
-    context "status available" do
+    context "value is available" do
       it "returns and cache the value" do
         parser    = klass.new(load_part('property_status_available.txt'))
         expected  = false
