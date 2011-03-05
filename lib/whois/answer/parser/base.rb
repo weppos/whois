3512e142d22cd0be04fa2e62b9708146582bb91e
Simone Carletti <weppos@weppos.net>
2011-03-05 18:16:21 +0100

    Array.wrap + #inject

--- lib/whois/answer/parser/base.rb
+++ lib/whois/answer/parser/base.rb
@@ -309,11 +309,10 @@ module Whois
         # @see Whois::Answer::Parser#contacts
         #
         def contacts
-          contacts = []
-          [:registrant_contact, :admin_contact, :technical_contact].each do |property|
-            contacts << send(property) if property_supported?(property)
+          [:registrant_contact, :admin_contact, :technical_contact].inject([]) do |contacts, property|
+            contacts += Array.wrap(send(property)) if property_supported?(property)
+            contacts
           end
-          contacts.flatten.compact
         end
 
         # @endgroup
