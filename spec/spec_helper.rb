77a8d4c162647988043bdb1e3cad6b487d64ffb5
Simone Carletti <weppos@weppos.net>
2011-03-16 20:59:23 +0100

    Make sure Ruby uses the right encoding and doesn't rely on ENV settings.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,15 @@ unless defined?(SPEC_ROOT)
   SPEC_ROOT = File.expand_path("../", __FILE__)
 end
 
+# The fixtures are UTF-8 encoded.
+# Make sure Ruby uses the proper encoding.
+if RUBY_VERSION < '1.9'
+  $KCODE='u'
+else
+  Encoding.default_external = Encoding::UTF_8
+  Encoding.default_internal = Encoding::UTF_8
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.join(SPEC_ROOT, "support/**/*.rb")].each { |f| require f }
