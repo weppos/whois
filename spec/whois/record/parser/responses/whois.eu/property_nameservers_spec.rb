dd472842b0801d4d69d24f7755d15776f48d3c21
Simone Carletti <weppos@weppos.net>
2011-08-04 17:35:05 +0200

    Make sure the `whois.eu` tests include both with/without IP name servers (references #99)

--- spec/whois/record/parser/responses/whois.eu/property_nameservers_spec.rb
+++ spec/whois/record/parser/responses/whois.eu/property_nameservers_spec.rb
@@ -21,16 +21,24 @@ describe Whois::Record::Parser::WhoisEu, "property_nameservers.expected" do
     @parser = klass.new(part)
   end
 
-  context "#nameservers" do
+  describe "#nameservers" do
     it do
       @parser.nameservers.should be_a(Array)
-      @parser.nameservers.should have(3).items
+      @parser.nameservers.should have(5).items
       @parser.nameservers[0].should be_a(_nameserver)
       @parser.nameservers[0].name.should == "a.nic.eu"
       @parser.nameservers[1].should be_a(_nameserver)
       @parser.nameservers[1].name.should == "l.nic.eu"
       @parser.nameservers[2].should be_a(_nameserver)
       @parser.nameservers[2].name.should == "p.nic.eu"
+      @parser.nameservers[3].should be_a(_nameserver)
+      @parser.nameservers[3].name.should == "ns1.eurid.eu"
+      @parser.nameservers[3].ipv4.should == "91.220.191.220"
+      @parser.nameservers[3].ipv6.should == nil
+      @parser.nameservers[4].should be_a(_nameserver)
+      @parser.nameservers[4].name.should == "ns2.eurid.eu"
+      @parser.nameservers[4].ipv4.should == "195.234.53.220"
+      @parser.nameservers[4].ipv6.should == nil
     end
   end
 end
