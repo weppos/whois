weppos/whois

commit ffbbaff8127ebc1a18d9f96d99d935bfa4fab71e

Fixed incompatibility with Ruby 1.8.7 resolv-replace and :bind_host (references #40).
When bind_host is not passed, the following error is raised: 

    ArgumentError: cannot interpret as DNS name: nil
    
    /usr/local/lib/ruby/1.8/resolv.rb:1060:in `create'
    /usr/local/lib/ruby/1.8/resolv.rb:906:in `generate_candidates'
    /usr/local/lib/ruby/1.8/resolv.rb:931:in `resolv'
    /usr/local/lib/ruby/1.8/resolv.rb:481:in `each_resource'
    /usr/local/lib/ruby/1.8/resolv.rb:386:in `each_address'
    /usr/local/lib/ruby/1.8/resolv.rb:115:in `each_address'
    /usr/local/lib/ruby/1.8/resolv.rb:114:in `each'
    /usr/local/lib/ruby/1.8/resolv.rb:114:in `each_address'
    /usr/local/lib/ruby/1.8/resolv.rb:92:in `getaddress'
    /usr/local/lib/ruby/1.8/resolv.rb:43:in `getaddress'
    /usr/local/lib/ruby/1.8/resolv-replace.rb:8:in `getaddress'
    /usr/local/lib/ruby/1.8/resolv-replace.rb:18:in `initialize'

lib/whois/server/adapters/base.rb
@@ -28,6 +28,10 @@ module Whois
         # Default WHOIS request port.
         DEFAULT_WHOIS_PORT = 43
 
+        # Default bind hostname.
+        DEFAULT_BIND_HOST = "0.0.0.0"
+
+
         # @return [Symbol] The type of WHOIS server
         attr_reader :type
         # @return [String] The allocation this server is responsible for.
@@ -150,7 +154,7 @@ module Whois
               query,
               host,
               port || options[:port] || DEFAULT_WHOIS_PORT,
-              options[:bind_host],
+              options[:bind_host] || DEFAULT_BIND_HOST,
               options[:bind_port]
             )
           end
@@ -162,6 +166,7 @@ module Whois
           #
           # @api internal
           def ask_the_socket(query, host, port, local_host, local_port)
+            args   = [host, port, local_host, local_port].compact
             client = TCPSocket.open(host, port, local_host, local_port)
             client.write("#{query}\r\n")    # I could use put(foo) and forget the \n
             client.read                     # but write/read is more symmetric than puts/read

spec/integration_spec.rb
@@ -6,7 +6,7 @@ describe Whois do
     it "works" do
       with_definitions do
         Whois::Server.define(:tld, ".it", "whois.nic.it")
-        Whois::Server::Adapters::Standard.any_instance.expects(:ask_the_socket).with("example.it", "whois.nic.it", 43, nil, nil).returns(<<-EOS)
+        Whois::Server::Adapters::Standard.any_instance.expects(:ask_the_socket).with("example.it", "whois.nic.it", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(<<-EOS)
   Domain:             example.it
   Status:             AVAILABLE
         EOS

spec/whois/server/adapters/afilias_spec.rb
@@ -13,7 +13,7 @@ describe Whois::Server::Adapters::Afilias do
       it "returns the WHOIS record" do
         response = "No match for DOMAIN.TEST."
         expected = response
-        @server.expects(:ask_the_socket).with("domain.test", "whois.afilias-grs.info", 43, nil, nil).returns(response)
+        @server.expects(:ask_the_socket).with("domain.test", "whois.afilias-grs.info", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
 
         record = @server.query("domain.test")
         record.to_s.should  == expected
@@ -27,8 +27,8 @@ describe Whois::Server::Adapters::Afilias do
         referral = File.read(fixture("referrals/afilias.bz.txt"))
         response = "Match for DOMAIN.TEST."
         expected = referral + "\n" + response
-        @server.expects(:ask_the_socket).with("domain.test", "whois.afilias-grs.info", 43, nil, nil).returns(referral)
-        @server.expects(:ask_the_socket).with("domain.test", "whois.belizenic.bz", 43, nil, nil).returns(response)
+        @server.expects(:ask_the_socket).with("domain.test", "whois.afilias-grs.info", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(referral)
+        @server.expects(:ask_the_socket).with("domain.test", "whois.belizenic.bz", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
 
         record = @server.query("domain.test")
         record.to_s.should  == expected

spec/whois/server/adapters/formatted_spec.rb
@@ -12,7 +12,7 @@ describe Whois::Server::Adapters::Formatted do
       response = "Whois Response"
       expected = response
       server = klass.new(*@definition)
-      server.expects(:ask_the_socket).with("-T dn,ace -C US-ASCII domain.de", "whois.denic.de", 43, nil, nil).returns(response)
+      server.expects(:ask_the_socket).with("-T dn,ace -C US-ASCII domain.de", "whois.denic.de", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
 
       record = server.query("domain.de")
       record.to_s.should  == expected
@@ -33,7 +33,7 @@ describe Whois::Server::Adapters::Formatted do
       it "sends the request to given port" do
         response = "Whois Response"
         server = klass.new(:tld, ".de", "whois.denic.de", { :format => "-T dn,ace -C US-ASCII %s", :port => 20 })
-        server.expects(:ask_the_socket).with("-T dn,ace -C US-ASCII domain.de", "whois.denic.de", 20, nil, nil).returns(response)
+        server.expects(:ask_the_socket).with("-T dn,ace -C US-ASCII domain.de", "whois.denic.de", 20, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
 
         server.query("domain.de")
       end

spec/whois/server/adapters/pir_spec.rb
@@ -13,7 +13,7 @@ describe Whois::Server::Adapters::Pir do
       it "returns the WHOIS record" do
         response = "No match for DOMAIN.TEST."
         expected = response
-        @server.expects(:ask_the_socket).with("FULL domain.test", "whois.publicinterestregistry.net", 43, nil, nil).returns(response)
+        @server.expects(:ask_the_socket).with("FULL domain.test", "whois.publicinterestregistry.net", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
 
         record = @server.query("domain.test")
         record.to_s.should  == expected
@@ -27,8 +27,8 @@ describe Whois::Server::Adapters::Pir do
         referral = File.read(fixture("referrals/pir.org.txt"))
         response = "Match for DOMAIN.TEST."
         expected = referral + "\n" + response
-        @server.expects(:ask_the_socket).with("FULL domain.test", "whois.publicinterestregistry.net", 43, nil, nil).returns(referral)
-        @server.expects(:ask_the_socket).with("domain.test", "whois.iana.org", 43, nil, nil).returns(response)
+        @server.expects(:ask_the_socket).with("FULL domain.test", "whois.publicinterestregistry.net", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(referral)
+        @server.expects(:ask_the_socket).with("domain.test", "whois.iana.org", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
 
         record = @server.query("domain.test")
         record.to_s.should  == expected

spec/whois/server/adapters/standard_spec.rb
@@ -12,7 +12,7 @@ describe Whois::Server::Adapters::Standard do
       response = "Whois Response"
       expected = response
       server = klass.new(*@definition)
-      server.expects(:ask_the_socket).with("domain.test", "whois.test", 43, nil, nil).returns(response)
+      server.expects(:ask_the_socket).with("domain.test", "whois.test", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
 
       record = server.query("domain.test")
       record.to_s.should  == expected
@@ -23,7 +23,7 @@ describe Whois::Server::Adapters::Standard do
       it "sends the request to given port" do
         response = "Whois Response"
         server = klass.new(:tld, ".test", "whois.test", { :port => 20 })
-        server.expects(:ask_the_socket).with("domain.test", "whois.test", 20, nil, nil).returns(response)
+        server.expects(:ask_the_socket).with("domain.test", "whois.test", 20, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
 
         server.query("domain.test")
       end

commit f64800bf3875306a17af628448fb230ac398f4c6

Fixed `Errno::EINVAL: Invalid argument - bind(2)' error that occasionally might occur connecting to a WHOIS server.
This bug is related to issue #40. In fact, the cause of this bug is the workaround introduced in commit ffbbaff8127ebc1a18d9f96d99d935bfa4fab71e to solve the incompatibility with TCPSocket.new when resolv-replace.rb is required and you're trying to pass a nil value as :local_host argument to TCPSocket.new. The workaround was to always default the :local_host to "0.0.0.0", that obviously means localhost. Unfortunately,

This solution works quite well as long as there are not connectivity or server issues. In fact, when for whatever reason "0.0.0.0" can't connect to the remote host, you get an error `Errno::EINVAL: Invalid argument - bind(2)'. Because I don't wont to add extra-complexity when not required, I decided to revert to the original code and default :local_host and :local_port to nil when they are not present. But hey, an other issue is just around the corner!

It turns that resolv-replace.rb tries to resolve the :local_host even if it's nil (ruby bug?) because it checks the existence of any local parameter, regardless you are providing :local_port or local_host.

    class TCPSocket < IPSocket
      # :stopdoc:
      alias original_resolv_initialize initialize
      # :startdoc:
      def initialize(host, serv, *rest)
        rest[0] = IPSocket.getaddress(rest[0]) unless rest.empty?
        original_resolv_initialize(IPSocket.getaddress(host), serv, *rest)
      end
    end

While the following statement is perfectly fine with TCPSocket

    TCPSocket.new("whois.nic.it", 43, nil, nil)

it doesn't work when you require resolv-replace.rb.

    ArgumentError: cannot interpret as DNS name: nil
    	from /Users/weppos/.rvm/rubies/ruby-1.9.2-p290/lib/ruby/1.9.1/resolv.rb:1121:in `create'
    	from /Users/weppos/.rvm/rubies/ruby-1.9.2-p290/lib/ruby/1.9.1/resolv.rb:967:in `generate_candidates'

lib/whois/server/adapters/base.rb
@@ -154,13 +154,27 @@ module Whois
 
           # @api public
           def query_the_socket(query, host, port = nil)
-            ask_the_socket(
-              query,
-              host,
-              port || options[:port] || DEFAULT_WHOIS_PORT,
-              options[:bind_host] || DEFAULT_BIND_HOST,
-              options[:bind_port]
-            )
+            args = []
+            args.push(host)
+            args.push(port || options[:port] || DEFAULT_WHOIS_PORT)
+
+            # This is a hack to prevent +TCPSocket.new+ to crash
+            # when resolv-replace.rb file is required.
+            #
+            # +TCPSocket.new+ defaults +local_host+ and +local_port+ to nil
+            # but when you require resolv-replace.rb, +local_host+
+            # is resolved when you pass any local parameter and in case of nil
+            # it raises the following error
+            #
+            #   ArgumentError: cannot interpret as DNS name: nil
+            #
+            if options[:bind_host] || options[:bind_port]
+              args.push(options[:bind_host] || DEFAULT_BIND_HOST)
+              args.push(options[:bind_port]) if options[:bind_port]
+            end
+
+            ask_the_socket(query, *args)
+
           rescue *RESCUABLE_CONNECTION_ERRORS => error
             raise ConnectionError, "#{error.class}: #{error.message}"
           end
@@ -171,9 +185,8 @@ module Whois
           # This is for internal use only!
           #
           # @api internal
-          def ask_the_socket(query, host, port, local_host, local_port)
-            args   = [host, port, local_host, local_port].compact
-            client = TCPSocket.open(host, port, local_host, local_port)
+          def ask_the_socket(query, *args)
+            client = TCPSocket.new(*args)
             client.write("#{query}\r\n")    # I could use put(foo) and forget the \n
             client.read                     # but write/read is more symmetric than puts/read
           ensure                            # and I really want to use read instead of gets.

spec/integration_spec.rb
@@ -2,14 +2,16 @@ require "spec_helper"
 
 describe Whois do
 
+  let(:response)    { "Domain:             example.it\nStatus:             AVAILABLE\n" }
+
   describe "Basic WHOIS querying and parsing" do
     it "works" do
       with_definitions do
         Whois::Server.define(:tld, ".it", "whois.nic.it")
-        Whois::Server::Adapters::Standard.any_instance.expects(:ask_the_socket).with("example.it", "whois.nic.it", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(<<-EOS)
-  Domain:             example.it
-  Status:             AVAILABLE
-        EOS
+        Whois::Server::Adapters::Standard.any_instance \
+            .expects(:ask_the_socket) \
+            .with("example.it", "whois.nic.it", 43) \
+            .returns(response)
 
         record = Whois.query("example.it")
 
@@ -23,17 +25,32 @@ describe Whois do
     end
   end
 
-  describe "Binding a WHOIS query to a different local host and port" do
-    it "works" do
+  describe "Passing :bind_host and :bind_port options" do
+    it "binds the WHOIS query to given host and port" do
       with_definitions do
         Whois::Server.define(:tld, ".it", "whois.nic.it")
-        Whois::Server::Adapters::Standard.any_instance.expects(:ask_the_socket).with("example.it", "whois.nic.it", 43, "127.0.0.1", 3000).returns(<<-EOS)
-  Domain:             example.it
-  Status:             AVAILABLE
-        EOS
+        Whois::Server::Adapters::Standard.any_instance \
+            .expects(:ask_the_socket) \
+            .with("example.it", "whois.nic.it", 43, "192.168.1.1", 3000) \
+            .returns(response)
 
-        client = Whois::Client.new(:bind_host => "127.0.0.1", :bind_port => 3000)
-        record = client.query("example.it")
+        client = Whois::Client.new(:bind_host => "192.168.1.1", :bind_port => 3000)
+        client.query("example.it")
+      end
+    end
+  end
+
+  describe "Passing :bind_port options" do
+    it "binds the WHOIS query to given port and defaults host" do
+      with_definitions do
+        Whois::Server.define(:tld, ".it", "whois.nic.it")
+        Whois::Server::Adapters::Standard.any_instance \
+            .expects(:ask_the_socket) \
+            .with("example.it", "whois.nic.it", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, 3000) \
+            .returns(response)
+
+        client = Whois::Client.new(:bind_port => 3000)
+        client.query("example.it")
       end
     end
   end

spec/whois/server/adapters/afilias_spec.rb
@@ -13,7 +13,7 @@ describe Whois::Server::Adapters::Afilias do
       it "returns the WHOIS record" do
         response = "No match for DOMAIN.TEST."
         expected = response
-        @server.expects(:ask_the_socket).with("domain.test", "whois.afilias-grs.info", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
+        @server.expects(:ask_the_socket).with("domain.test", "whois.afilias-grs.info", 43).returns(response)
 
         record = @server.query("domain.test")
         record.to_s.should  == expected
@@ -27,8 +27,8 @@ describe Whois::Server::Adapters::Afilias do
         referral = File.read(fixture("referrals/afilias.bz.txt"))
         response = "Match for DOMAIN.TEST."
         expected = referral + "\n" + response
-        @server.expects(:ask_the_socket).with("domain.test", "whois.afilias-grs.info", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(referral)
-        @server.expects(:ask_the_socket).with("domain.test", "whois.belizenic.bz", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
+        @server.expects(:ask_the_socket).with("domain.test", "whois.afilias-grs.info", 43).returns(referral)
+        @server.expects(:ask_the_socket).with("domain.test", "whois.belizenic.bz", 43).returns(response)
 
         record = @server.query("domain.test")
         record.to_s.should  == expected

spec/whois/server/adapters/base_spec.rb
@@ -2,9 +2,7 @@ require "spec_helper"
 
 describe Whois::Server::Adapters::Base do
 
-  before(:each) do
-    @definition = [:tld, ".test", "whois.test", { :foo => "bar" }]
-  end
+  let(:definition)  { [:tld, ".test", "whois.test", { :foo => "bar" }] }
 
 
   describe "#initialize" do
@@ -34,14 +32,14 @@ describe Whois::Server::Adapters::Base do
 
   describe "#==" do
     it "returns true when other is the same instance" do
-      one = two = klass.new(*@definition)
+      one = two = klass.new(*definition)
 
       (one == two).should be_true
       (one.eql? two).should be_true
     end
 
     it "returns true when other has same class and has the same attributes" do
-      one, two = klass.new(*@definition), klass.new(*@definition)
+      one, two = klass.new(*definition), klass.new(*definition)
 
       (one == two).should be_true
       (one.eql? two).should be_true
@@ -49,14 +47,14 @@ describe Whois::Server::Adapters::Base do
 
     it "returns true when other has descendant class and has the same attributes" do
       subklass = Class.new(klass)
-      one, two = klass.new(*@definition), subklass.new(*@definition)
+      one, two = klass.new(*definition), subklass.new(*definition)
 
       (one == two).should be_true
       (one.eql? two).should be_true
     end
 
     it "returns false when other has different class and has the same attributes" do
-      one, two = klass.new(*@definition), Struct.new(:type, :allocation, :host, :options).new(*@definition)
+      one, two = klass.new(*definition), Struct.new(:type, :allocation, :host, :options).new(*definition)
 
       (one == two).should be_false
       (one.eql? two).should be_false
@@ -96,13 +94,13 @@ describe Whois::Server::Adapters::Base do
 
   describe "#query" do
     it "raises NotImplementedError" do
-      lambda { klass.new(*@definition).query("example.test") }.should raise_error(NotImplementedError)
+      lambda { klass.new(*definition).query("example.test") }.should raise_error(NotImplementedError)
     end
   end
 
   describe "#request" do
     it "is an abstract method" do
-      lambda { klass.new(*@definition).request("example.test") }.should raise_error(NotImplementedError)
+      lambda { klass.new(*definition).request("example.test") }.should raise_error(NotImplementedError)
     end
   end
 
@@ -111,8 +109,50 @@ describe Whois::Server::Adapters::Base do
       it "re-raises #{error} as Whois::ConnectionError" do
         klass.any_instance.expects(:ask_the_socket).raises(error)
         expect {
-          klass.new(*@definition).send(:query_the_socket, "example.com", "whois.test")
+          klass.new(*definition).send(:query_the_socket, "example.com", "whois.test")
         }.to raise_error(Whois::ConnectionError, "#{error}: #{error.new.message}")
+      end
+    end
+
+    context "without :bind_host or :bind_port options" do
+      before(:each) do
+        @base = klass.new(:tld, ".test", "whois.test", {})
+      end
+
+      it "does not bind the WHOIS query" do
+        @base \
+            .expects(:ask_the_socket) \
+            .with("example.test", "whois.test", 43)
+
+        @base.send(:query_the_socket, "example.test", "whois.test", 43)
+      end
+    end
+
+    context "with :bind_host and :bind_port options" do
+      before(:each) do
+        @base = klass.new(:tld, ".test", "whois.test", { :bind_host => "192.168.1.1", :bind_port => 3000 })
+      end
+
+      it "binds the WHOIS query to given host and port" do
+        @base \
+            .expects(:ask_the_socket) \
+            .with("example.test", "whois.test", 43, "192.168.1.1", 3000)
+
+        @base.send(:query_the_socket, "example.test", "whois.test", 43)
+      end
+    end
+
+    context "with :bind_port and without :bind_host options" do
+      before(:each) do
+        @base = klass.new(:tld, ".test", "whois.test", { :bind_port => 3000 })
+      end
+
+      it "binds the WHOIS query to given port and defaults host" do
+        @base \
+            .expects(:ask_the_socket) \
+            .with("example.test", "whois.test", 43, klass::DEFAULT_BIND_HOST, 3000)
+
+        @base.send(:query_the_socket, "example.test", "whois.test", 43)
       end
     end
   end

spec/whois/server/adapters/formatted_spec.rb
@@ -12,7 +12,7 @@ describe Whois::Server::Adapters::Formatted do
       response = "Whois Response"
       expected = response
       server = klass.new(*@definition)
-      server.expects(:ask_the_socket).with("-T dn,ace -C US-ASCII domain.de", "whois.denic.de", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
+      server.expects(:ask_the_socket).with("-T dn,ace -C US-ASCII domain.de", "whois.denic.de", 43).returns(response)
 
       record = server.query("domain.de")
       record.to_s.should  == expected
@@ -33,7 +33,7 @@ describe Whois::Server::Adapters::Formatted do
       it "sends the request to given port" do
         response = "Whois Response"
         server = klass.new(:tld, ".de", "whois.denic.de", { :format => "-T dn,ace -C US-ASCII %s", :port => 20 })
-        server.expects(:ask_the_socket).with("-T dn,ace -C US-ASCII domain.de", "whois.denic.de", 20, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
+        server.expects(:ask_the_socket).with("-T dn,ace -C US-ASCII domain.de", "whois.denic.de", 20).returns(response)
 
         server.query("domain.de")
       end
@@ -43,8 +43,8 @@ describe Whois::Server::Adapters::Formatted do
       it "binds the request to given host and port" do
         response = "Whois Response"
         server = klass.new(:tld, ".de", "whois.denic.de", { :format => "-T dn,ace -C US-ASCII %s" })
-        server.configure(:bind_host => "192.168.0.100", :bind_port => 3000)
-        server.expects(:ask_the_socket).with("-T dn,ace -C US-ASCII domain.de", "whois.denic.de", 43, "192.168.0.100", 3000).returns(response)
+        server.configure(:bind_host => "192.168.1.1", :bind_port => 3000)
+        server.expects(:ask_the_socket).with("-T dn,ace -C US-ASCII domain.de", "whois.denic.de", 43, "192.168.1.1", 3000).returns(response)
 
         server.query("domain.de")
       end

spec/whois/server/adapters/pir_spec.rb
@@ -13,7 +13,7 @@ describe Whois::Server::Adapters::Pir do
       it "returns the WHOIS record" do
         response = "No match for DOMAIN.TEST."
         expected = response
-        @server.expects(:ask_the_socket).with("FULL domain.test", "whois.publicinterestregistry.net", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
+        @server.expects(:ask_the_socket).with("FULL domain.test", "whois.publicinterestregistry.net", 43).returns(response)
 
         record = @server.query("domain.test")
         record.to_s.should  == expected
@@ -27,8 +27,8 @@ describe Whois::Server::Adapters::Pir do
         referral = File.read(fixture("referrals/pir.org.txt"))
         response = "Match for DOMAIN.TEST."
         expected = referral + "\n" + response
-        @server.expects(:ask_the_socket).with("FULL domain.test", "whois.publicinterestregistry.net", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(referral)
-        @server.expects(:ask_the_socket).with("domain.test", "whois.iana.org", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
+        @server.expects(:ask_the_socket).with("FULL domain.test", "whois.publicinterestregistry.net", 43).returns(referral)
+        @server.expects(:ask_the_socket).with("domain.test", "whois.iana.org", 43).returns(response)
 
         record = @server.query("domain.test")
         record.to_s.should  == expected

spec/whois/server/adapters/standard_spec.rb
@@ -12,7 +12,7 @@ describe Whois::Server::Adapters::Standard do
       response = "Whois Response"
       expected = response
       server = klass.new(*@definition)
-      server.expects(:ask_the_socket).with("domain.test", "whois.test", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
+      server.expects(:ask_the_socket).with("domain.test", "whois.test", 43).returns(response)
 
       record = server.query("domain.test")
       record.to_s.should  == expected
@@ -23,7 +23,7 @@ describe Whois::Server::Adapters::Standard do
       it "sends the request to given port" do
         response = "Whois Response"
         server = klass.new(:tld, ".test", "whois.test", { :port => 20 })
-        server.expects(:ask_the_socket).with("domain.test", "whois.test", 20, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
+        server.expects(:ask_the_socket).with("domain.test", "whois.test", 20).returns(response)
 
         server.query("domain.test")
       end
@@ -33,8 +33,8 @@ describe Whois::Server::Adapters::Standard do
       it "binds the request to given host and port" do
         response = "Whois Response"
         server = klass.new(:tld, ".test", "whois.test", { :port => 20 })
-        server.configure(:bind_host => "192.168.0.100", :bind_port => 3000)
-        server.expects(:ask_the_socket).with("domain.test", "whois.test", 20, "192.168.0.100", 3000).returns(response)
+        server.configure(:bind_host => "192.168.1.100", :bind_port => 3000)
+        server.expects(:ask_the_socket).with("domain.test", "whois.test", 20, "192.168.1.100", 3000).returns(response)
 
         server.query("domain.test")
       end

spec/whois/server/adapters/verisign_spec.rb
@@ -13,7 +13,7 @@ describe Whois::Server::Adapters::Verisign do
       it "returns the WHOIS record" do
         response = "No match for DOMAIN.TEST."
         expected = response
-        @server.expects(:ask_the_socket).with("=domain.test", "whois.test", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
+        @server.expects(:ask_the_socket).with("=domain.test", "whois.test", 43).returns(response)
 
         record = @server.query("domain.test")
         record.to_s.should  == expected
@@ -27,8 +27,8 @@ describe Whois::Server::Adapters::Verisign do
         referral = File.read(fixture("referrals/crsnic.com.txt"))
         response = "Match for DOMAIN.TEST."
         expected = referral + "\n" + response
-        @server.expects(:ask_the_socket).with("=domain.test", "whois.test", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(referral)
-        @server.expects(:ask_the_socket).with("domain.test", "whois.markmonitor.com", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(response)
+        @server.expects(:ask_the_socket).with("=domain.test", "whois.test", 43).returns(referral)
+        @server.expects(:ask_the_socket).with("domain.test", "whois.markmonitor.com", 43).returns(response)
 
         record = @server.query("domain.test")
         record.to_s.should  == expected
@@ -38,8 +38,8 @@ describe Whois::Server::Adapters::Verisign do
 
       it "extracts the closest referral when multiple referrals" do
         referral = File.read(fixture("referrals/crsnic.com_referral_multiple.txt"))
-        @server.expects(:ask_the_socket).with("=domain.test", "whois.test", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(referral)
-        @server.expects(:ask_the_socket).with("domain.test", "whois.markmonitor.com", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns("")
+        @server.expects(:ask_the_socket).with("=domain.test", "whois.test", 43).returns(referral)
+        @server.expects(:ask_the_socket).with("domain.test", "whois.markmonitor.com", 43).returns("")
 
         record = @server.query("domain.test")
         record.parts.should have(2).parts
@@ -47,7 +47,7 @@ describe Whois::Server::Adapters::Verisign do
 
       it "ignores referral when is not defined" do
         referral = File.read(fixture("referrals/crsnic.com_referral_not_defined.txt"))
-        @server.expects(:ask_the_socket).with("=domain.test", "whois.test", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(referral)
+        @server.expects(:ask_the_socket).with("=domain.test", "whois.test", 43).returns(referral)
         @server.expects(:ask_the_socket).never
 
         record = @server.query("domain.test")
@@ -58,7 +58,7 @@ describe Whois::Server::Adapters::Verisign do
       # This is the case of vrsn-20100925-dnssecmonitor86.net
       it "gracefully ignores referral when is missing" do
         referral = File.read(fixture("referrals/crsnic.com_referral_missing.txt"))
-        @server.expects(:ask_the_socket).with("=domain.test", "whois.test", 43, Whois::Server::Adapters::Base::DEFAULT_BIND_HOST, nil).returns(referral)
+        @server.expects(:ask_the_socket).with("=domain.test", "whois.test", 43).returns(referral)
         @server.expects(:ask_the_socket).never
 
         record = @server.query("domain.test")