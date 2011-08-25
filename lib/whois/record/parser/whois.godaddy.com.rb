6d7c0a3c9ee95c840ebe7e9883fda3c98a68cb23
Simone Carletti <weppos@weppos.net>
2011-08-25 14:11:33 +0200

    Cleaned up whois.godaddy.com parser

--- lib/whois/record/parser/whois.godaddy.com.rb
+++ lib/whois/record/parser/whois.godaddy.com.rb
@@ -14,25 +14,23 @@ module Whois
   class Record
     class Parser
 
-      #
-      # = whois.godaddy.com parser
-      #
       # Parser for the whois.godaddy.com server.
-      # Contributed by Tom Nicholls <tom.nicholls@oii.ox.ac.uk>
       #
+      # @note This parser is just a stub and provides only a few basic methods
+      #   to check for domain availability and get domain status.
+      #   Please consider to contribute implementing missing methods.
+      # 
+      # @see Whois::Record::Parser::Example
+      #   The Example parser for the list of all available methods.
       #
-      # NOTE: This parser is just a stub and provides only a few basic methods
-      # to check for domain availability and get domain status.
-      # Please consider to contribute implementing missing methods.
-      # See WhoisNicIt parser for an explanation of all available methods
-      # and examples.
-      #
-
+      # @author Simone Carletti
+      # @author Tom Nicholls <tom.nicholls@oii.ox.ac.uk>
+      # @since  GIT:MASTER
       class WhoisGodaddyCom < Base
 
         property_not_supported :status
 
-        # The server seems to provide only information for registered domains
+        # The server seems to provide only linesrmation for registered domains
         property_supported :available? do
           false
         end
@@ -81,8 +79,10 @@ module Whois
         end
 
         property_supported :nameservers do
-          content_for_scanner[/Domain servers in listed order:\n((?:\s*[^\s\n]+\n)+)\n\s*\n/, 1].split("\n").map do |line|
-            Record::Nameserver.new(line.strip)
+          if content_for_scanner =~ /Domain servers in listed order:\n((.+\n)+)\n/
+            $1.split("\n").map do |line|
+              Record::Nameserver.new(line.strip)
+            end
           end
         end
 
@@ -91,36 +91,42 @@ module Whois
 
 
           def contact(element, type)
-            info = content_for_scanner[/#{element}\n((.+\n)+)\n/, 1].to_s.split("\n").map(&:strip)
-            # NB: Lines 1 and 5 may be absent, depending on the record.
-            # The parser attempts to correct for this, but may be a bit flaky
-            # on non-standard data.
-            # 0 GoDaddy.com, Inc., GoDaddy.com, Inc.  dns@jomax.net
-            # 1 GoDaddy.com, Inc.
-            # 2 14455 N Hayden Rd Suite 219
-            # 3 Scottsdale, Arizona 85260
-            # 4 United States
-            # 5 +1.4805058800      Fax -- +1.4805058844
-            phone = ""
-	    fax = ""
-            if info[-1].to_s =~ /Fax --/
-               phone, fax = info.delete_at(-1).to_s.scan(/^(.*) Fax --(.*)$/).first
-               phone = phone.strip
-               fax = fax.strip
+            if content_for_scanner =~ /#{element}\n((.+\n)+)\n/
+              lines = $1.split("\n").map(&:strip)
+
+              # Lines 1 and 5 may be absent, depending on the record.
+              # The parser attempts to correct for this, but may be a bit flaky
+              # on non-standard data.
+              #
+              #   GoDaddy.com, Inc., GoDaddy.com, Inc.  dns@jomax.net
+              #   GoDaddy.com, Inc.
+              #   14455 N Hayden Rd Suite 219
+              #   Scottsdale, Arizona 85260
+              #   United States
+              #   +1.4805058800      Fax -- +1.4805058844
+              phone = nil
+              fax   = nil
+              if lines[-1].to_s =~ /Fax --/
+                 phone, fax = lines.delete_at(-1).to_s.scan(/^(.*) Fax --(.*)$/).first
+                 phone = phone.strip
+                 fax   = fax.strip
+              end
+
+              Record::Contact.new(
+                :type         => type,
+                :id           => nil,
+                :name         => lines[0].to_s.gsub(/\s\S+@[^\.].*\.[a-z]{2,}\s?\)?$/, "").strip,
+                :organization => lines.length >= 5 ? lines[-4] : "",
+                :address      => lines.length >= 4 ? lines[-3] : "",
+                :city         => lines.length >= 4 ? lines[-2].to_s.partition(",")[0] : "",
+                :zip          => lines.length >= 4 ? lines[-2].to_s.rpartition(" ")[2] : "",
+                :state        => lines.length >= 4 ? lines[-2].to_s.partition(",")[2].rpartition(" ")[0].to_s.strip : "",
+                :country      => lines.length >= 4 ? lines[-1] : "",
+                :phone        => phone,
+                :fax          => fax,
+                :email        => lines[0].to_s.scan(/[^\s]\S+@[^\.].*\.[a-z]{2,}[^\s\)\n]/).first
+              )
             end
-            Record::Contact.new(
-              :type => type,
-              :name => info[0].to_s.gsub(/\s\S+@[^\.].*\.[a-z]{2,}\s?\)?$/, "").strip,
-              :organization => info.length >= 5 ? info[-4] : "",
-              :address => info.length >= 4 ? info[-3] : "",
-              :city => info.length >= 4 ? info[-2].to_s.partition(",")[0] : "",
-              :state => info.length >= 4 ? info[-2].to_s.partition(",")[2].rpartition(" ")[0].to_s.strip : "",
-              :zip => info.length >= 4 ? info[-2].to_s.rpartition(" ")[2] : "",
-              :country_code => info.length >= 4 ? info[-1] : "",
-              :email => info[0].to_s.scan(/[^\s]\S+@[^\.].*\.[a-z]{2,}[^\s\)\n]/).first,
-              :phone => phone,
-              :fax => fax
-            )
           end
 
       end
