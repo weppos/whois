afd4527e8087a9d7c866175ff98b64622a87646e
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2012-03-20 16:21:57 +0300

    Added whois.cctld.by ast scanner and implemented properties status, registered? and available?

---
+++ lib/whois/record/parser/scanners/whois.cctld.by.rb
@@ -0,0 +1,49 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2012 Simone Carletti <weppos@weppos.net>
+#++
+
+require 'whois/record/parser/scanners/base'
+
+module Whois
+  class Record
+    class Parser
+      module Scanners
+
+        #
+        # = whois.cctld.by scanner
+        #
+        # Ast scanner for the whois.cctld.by server.
+        #
+        # @author Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
+        class WhoisCctldBy < Base
+
+          self.tokenizers += [
+            :skip_empty_line,
+            :scan_available,
+            :scan_keyvalue,
+            :skip_empty_line,
+            :skip_provider_signature,
+          ]
+
+          tokenizer :scan_available do
+            if @input.scan(/^Object does not exist/)
+              @ast["status:available"] = true
+            end
+          end
+
+          tokenizer :skip_lastupdate do
+            @input.skip(/>>>(.+?)<<<\n/)
+          end
+
+          tokenizer :skip_provider_signature do
+            @input.scan(/^\S(.+)\n/)
+          end
+        end
+      end
+    end
+  end
+end
--- lib/whois/record/parser/whois.cctld.by.rb
+++ lib/whois/record/parser/whois.cctld.by.rb
@@ -7,6 +7,7 @@
 #++
 
 require 'whois/record/parser/base'
+require 'whois/record/parser/scanners/whois.cctld.by'
 
 module Whois
   class Record
@@ -25,55 +26,61 @@ module Whois
       #
       # @author Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
       class WhoisCctldBy < Base
+        include Scanners::Ast
+
+        # Initializes a new {Scanners::WhoisCctldBy} instance
+        # passing the {#content_for_scanner}
+        # and calls +parse+ on it.
+        #
+        # @return [Hash]
+        def parse
+          Scanners::WhoisCctldBy.new(content_for_scanner).parse
+        end
+
+        property_supported :available? do
+          !!node("status:available")
+        end
+
+        property_supported :registered? do
+          !available?
+        end
+
+        property_supported :domain do
+          node("Domain Name", &:downcase)
+        end
+
+        property_supported :status do
+          if available?
+            :available
+          else
+            :registered
+          end
+        end
+
+        #property_not_supported :created_on
+        #
+        #property_not_supported :updated_on
+        #
+        #property_not_supported :expires_on
+
+        #property_not_supported :registrar
+        #
+        #property_not_supported :nameservers
+
         property_not_supported :disclaimer
-
-        property_not_supported :domain
-
-        property_not_supported :domain_id
-
 
         property_not_supported :referral_whois
 
         property_not_supported :referral_url
 
-
-        #property_supported :status do
-        #  if available?
-        #    :available
-        #  else
-        #    :registered
-        #  end
-        #end
-        #
-        #property_supported :available? do
-        #  content_for_scanner.strip == "Available"
-        #end
-        #
-        #property_supported :registered? do
-        #  !available?
-        #end
-
-
-        property_not_supported :created_on
-
-        property_not_supported :updated_on
-
-        property_not_supported :expires_on
-
-
-        property_not_supported :registrar
+        property_not_supported :domain_id
 
         property_not_supported :registrant_contacts
 
         property_not_supported :admin_contacts
 
         property_not_supported :technical_contacts
-
-
-        property_not_supported :nameservers
-
       end
-
     end
   end
 end
