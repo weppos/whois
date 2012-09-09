f0124bab33e66e80b0c30cb175e7978000a35499
Simone Carletti <weppos@weppos.net>
2012-09-09 22:56:22 +0200

    The server is now whois1.nic.bi

---
+++ lib/whois/record/parser/whois1.nic.bi.rb
@@ -0,0 +1,23 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2012 Simone Carletti <weppos@weppos.net>
+#++
+
+
+require 'whois/record/parser/base_cocca'
+
+
+module Whois
+  class Record
+    class Parser
+
+      # Parser for the whois1.nic.bi server.
+      class Whois1NicBi < BaseCocca
+      end
+
+    end
+  end
+end
