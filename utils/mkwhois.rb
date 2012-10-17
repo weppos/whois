2c1ff9f03be5fd1e9865fe9805e53db14fe6510b
Simone Carletti <weppos@weppos.net>
2012-10-17 20:40:20 +0200

    Rename mkwhois

--- utils/mkwhois.rb
+++ utils/mkwhois.rb
@@ -2,8 +2,8 @@
 
 $:.unshift(File.expand_path("../../lib", __FILE__))
 
-require "fileutils"
-require "whois"
+require 'fileutils'
+require 'whois'
 begin
   require File.expand_path("../whois-utf8", __FILE__)
 rescue LoadError
@@ -15,8 +15,8 @@ n = ARGV.shift || raise("Missing file name")
 r = Whois.query(d)
 
 r.parts.each do |part|
-  p = File.expand_path("../../spec/fixtures/responses/#{part.host}/#{n}", __FILE__)
-  FileUtils.mkdir_p(File.dirname(p))
-  File.open(p, "w+") { |f| f.write(part.body) }
-  puts "Saved #{p}"
+  target = File.expand_path("../../spec/fixtures/responses/#{part.host}/#{n}.txt", __FILE__)
+  FileUtils.mkdir_p(File.dirname(target))
+  File.open(target, "w+") { |f| f.write(part.body) }
+  puts "Saved #{target}"
 end
