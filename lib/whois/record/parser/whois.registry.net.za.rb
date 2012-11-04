33970e22bdb16bdb7152e8f58926d3a53a064867
Rory McKinley <rorymckinley@gmail.com>
2012-11-04 16:19:19 +0200

    Update code documentation

--- lib/whois/record/parser/whois.registry.net.za.rb
+++ lib/whois/record/parser/whois.registry.net.za.rb
@@ -5,11 +5,15 @@ require 'whois/record/scanners/whois.registry.net.za'
 module Whois
   class Record
     class Parser
-      #--
+      # Parser for the whois.registry.za.net server.
+      #
       # @note This parser is just a stub and provides only a few basic methods
       #   to check for domain availability and get domain status.
       #   Please consider to contribute implementing missing methods.
-      # ++
+      #
+      # @see Whois::Record::Parser::Example
+      #   The Example parser for the list of all available methods.
+      #
       class WhoisRegistryNetZa < Base
         include Scanners::Nodable
 
