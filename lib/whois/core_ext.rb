0ceb4d7d319f2f2f12103d8c4e3087fe179fbfb7
Simone Carletti <weppos@weppos.net>
2012-11-04 13:04:59 -0700

    Ruby 1.9 defines DateTime#to_time

--- lib/whois/core_ext.rb
+++ lib/whois/core_ext.rb
@@ -1,9 +1,7 @@
 if defined?(ActiveSupport::VERSION) && ActiveSupport::VERSION::MAJOR == 3
   require 'active_support/core_ext/array/wrap'
-  require 'active_support/core_ext/date_time/conversions'
   require 'whois/core_ext/class/attribute'
 else
   require 'whois/core_ext/array/wrap'
-  require 'whois/core_ext/date_time/conversions'
   require 'whois/core_ext/class/attribute'
 end
