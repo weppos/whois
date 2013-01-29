3734b71495436e76304c7132e296aa4289c0d71b
yangsec888 <yangsec888@gmail.com>
2013-01-29 18:54:53 -0500

    implement new methods in parser whois.dk-hostmaster.dk.rb

--- lib/whois/record/parser/whois.dk-hostmaster.dk.rb
+++ lib/whois/record/parser/whois.dk-hostmaster.dk.rb
@@ -66,15 +66,55 @@ module Whois
           end
         end
 
-
         property_supported :nameservers do
           content_for_scanner.scan(/Hostname:\s+(.+)\n/).flatten.map do |name|
             Record::Nameserver.new(:name => name)
           end
         end
 
-      end
+		# The following methods are implemented by Yang Li on 01/29/2013
+		# ----------------------------------------------------------------------------
+        property_supported :domain do
+          return $1 if content_for_scanner =~ /Domain:\s+(.+)\n/i
+        end
+		
+		property_not_supported :domain_id
+		
+        property_not_supported :registrar 
+		
+		property_supported :admin_contacts do
+          build_contact("Administrator", Whois::Record::Contact::TYPE_ADMIN)		
+		end
 
+        property_supported :registrant_contacts do
+          build_contact("Registrant", Whois::Record::Contact::TYPE_REGISTRANT)
+        end
+
+        property_not_supported :technical_contacts 
+
+        property_not_supported :billing_contacts 
+		
+      private
+
+        def build_contact(element, type)
+          reg=Record::Contact.new(:type => type)
+		  if content_for_scanner =~ /^#{element}\n((.+\n)+)\n/i			
+			  $1.scan(/^(.+):\s+(.+)\n/).map do |entry|
+				  reg["id"]=entry[1] if entry[0] =~ /Handle/i
+				  reg["name"]=entry[1] if entry[0] =~ /Name/i
+				  reg["organization"]=entry[1] if entry[0]=~ /Name/i
+				  reg["address"]=entry[1] if entry[0]=~ /Address/i
+				  reg["city"]= entry[1] if entry[0]=~ /City/i
+				  reg["zip"]=entry[1] if entry[0]=~ /PostalCode/i
+				  reg["country_code"]=entry[1] if entry[0]=~ /Country/i
+				  reg["phone"]=entry[1] if entry[0]=~ /Phone/i
+			  end
+			end
+		  return reg
+        end	
+		# ----------------------------------------------------------------------------
+
+      end
     end
   end
 end
