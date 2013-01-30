a29994db6a03cdced7b82ba5b056ae0e4c8f3adb
yangsec888 <yangsec888@gmail.com>
2013-01-29 20:08:53 -0500

    implement new methods in the parser whois.nic.ve.rb

--- lib/whois/record/parser/whois.nic.ve.rb
+++ lib/whois/record/parser/whois.nic.ve.rb
@@ -77,13 +77,59 @@ module Whois
           end
         end
 
-
         # NEWPROPERTY
         # def suspended?
         # end
+		
+		# The following methods are implemented by Yang Li on 01/29/2013
+		# ----------------------------------------------------------------------------
+        property_supported :domain do
+          return $1 if content_for_scanner =~ /Nombre de Dominio:\s+(.*)\n/i
+        end
+		
+		property_not_supported :domain_id
+		
+        property_not_supported :registrar 
+		
+		property_supported :admin_contacts do
+          build_contact("Contacto Administrativo", Whois::Record::Contact::TYPE_ADMIN)
+        end
+		
+        property_supported :registrant_contacts do
+          build_contact("Titular", Whois::Record::Contact::TYPE_REGISTRANT)
+        end
+
+        property_supported :technical_contacts do
+          build_contact("Contacto Tecnico", Whois::Record::Contact::TYPE_TECHNICAL)
+        end
+
+        property_supported :billing_contacts do
+          build_contact("Contacto de Cobranza", Whois::Record::Contact::TYPE_BILLING)
+        end
+		
+      private
+
+        def build_contact(element, type)
+          reg=Record::Contact.new(:type => type)
+		  if content_for_scanner =~ /#{element}:\s*\n((.+\n)+)\n/i
+			  line_num=1
+			  $1.split(%r{\n}).each do |line| 
+				titles=line.strip if line_num==1
+				reg["name"]=line.strip.split(%r{\t+}).first if line_num==1
+				reg["email"]=line.strip.split(%r{\t+}).last if line_num==1
+				reg["organization"]=line.strip if line_num==2
+				reg["address"]=line.strip if line_num==3
+				reg["city"]=line.strip if line_num==4
+				reg["phone"]=line.strip.split(%r{\s+}).first if line_num==5
+				reg["fax"]=line.split(%r{\s+}).last if line_num==5
+				line_num=line_num+1
+			  end			  
+          end
+		  return reg
+        end	
+		# ----------------------------------------------------------------------------
 
       end
-
     end
   end
 end
