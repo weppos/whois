890cd452bdeab7d5b2a302f10b0b253413871d67
yangsec888 <yangsec888@gmail.com>
2013-02-05 14:02:08 -0500

    Implement new methods for the parser whois.nic.tr.rb

--- lib/whois/record/parser/whois.nic.tr.rb
+++ lib/whois/record/parser/whois.nic.tr.rb
@@ -6,9 +6,7 @@
 # Copyright (c) 2009-2012 Simone Carletti <weppos@weppos.net>
 #++
 
-
 require 'whois/record/parser/base'
-
 
 module Whois
   class Record
@@ -43,7 +41,6 @@ module Whois
           !available?
         end
 
-
         property_supported :created_on do
           if content_for_scanner =~ /Created on\.+:\s+(.+)\n/
             time = Time.parse($1)
@@ -59,8 +56,7 @@ module Whois
             Time.utc(time.year, time.month, time.day)
           end
         end
-
-
+		
         property_supported :nameservers do
           if content_for_scanner =~ /Domain Servers:\n((.+\n)+)\n/
             $1.split("\n").map do |line|
@@ -70,8 +66,68 @@ module Whois
           end
         end
 
-      end
+		# The following methods are implemented by Yang Li on 02/05/2013
+		# ----------------------------------------------------------------------------
+        property_not_supported :domain 
+		
+		property_not_supported :domain_id
+		
+        property_not_supported :registrar 
+		
+		property_supported :admin_contacts do
+          build_contact("Administrative Contact", Whois::Record::Contact::TYPE_ADMIN)
+        end
 
+        property_supported :registrant_contacts do
+          build_contact_1("Registrant", Whois::Record::Contact::TYPE_REGISTRANT)
+        end
+
+        property_supported :technical_contacts do
+          build_contact("Technical Contact", Whois::Record::Contact::TYPE_TECHNICAL)
+        end
+
+        property_supported :billing_contacts do
+          build_contact("Billing Contact", Whois::Record::Contact::TYPE_BILLING)
+        end
+		
+      private
+
+        def build_contact(element, type)
+          reg=Record::Contact.new(:type => type)
+		  if content_for_scanner =~ /#{element}:\n((.+\n)+)\n\n/i
+			  $1.scan(/(.+):(.+)\n/).map do |entry|
+				  reg["id"]=entry[1].strip if entry[0] =~ /NIC Handle/i
+				  reg["name"]=entry[1].strip if entry[0] =~ /Name/i
+				  reg["organization"]=entry[1].strip if entry[0]=~ /Organization/i
+				  reg["address"]=entry[1].strip if entry[0]=~ /Address/i
+				  reg["phone"]=entry[1].strip if entry[0]=~ /Phone/i
+				  reg["fax"]=entry[1].strip if entry[0]=~ /Fax/i
+			  end			  
+          end
+		  return reg
+        end	
+		
+        def build_contact_1(element, type)
+          reg=Record::Contact.new(:type => type)
+		  if content_for_scanner =~ /#{element}:\n((.+\n)+)\n\n/i
+			  line_num=1
+			  $1.split(%r{\n}).each do |line|
+				reg["name"]=line.strip if line_num==1
+				reg["organization"]=line.strip if line_num==1
+				reg["address"]=line.strip if line_num==2
+				reg["city"]=line.strip if line_num==3
+				reg["country"]=line.strip if line_num==5
+				reg["email"]=line.strip if line=~ /\w+\@\w+\.\w+/
+				reg["phone"]=line.strip if line_num==7
+				reg["fax"]=line.strip if line_num==8
+				line_num=line_num+1
+			  end					  
+          end
+		  return reg
+        end	
+		# ----------------------------------------------------------------------------
+		
+      end
     end
   end
 end
