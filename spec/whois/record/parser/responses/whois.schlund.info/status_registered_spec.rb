f1a5debf4c1cb446ae2a09c0a3baf8eebb8264ca
Simone Carletti <weppos@weppos.net>
2013-11-11 14:17:59 +0100

    The return value of status should be a Symbol status

--- spec/whois/record/parser/responses/whois.schlund.info/status_registered_spec.rb
+++ spec/whois/record/parser/responses/whois.schlund.info/status_registered_spec.rb
@@ -33,7 +33,7 @@ describe Whois::Record::Parser::WhoisSchlundInfo, "status_registered.expected" d
   end
   describe "#status" do
     it do
-      expect(subject.status).to eq(["clientTransferProhibited"])
+      expect(subject.status).to eq(:registered)
     end
   end
   describe "#available?" do
