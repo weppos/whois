5c137ec9d0b2d5de9e8133ab7f2b3611360a22d9
Huy Phan <dachuy@gmail.com>
2014-03-18 16:21:27 +0800

    Add whois.corporatedomains.com parser

---
+++ lib/whois/record/parser/whois.corporatedomains.com.rb
@@ -0,0 +1,30 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2014 Simone Carletti <weppos@weppos.net>
+#++
+
+
+require 'whois/record/parser/base_icann_compliant'
+
+
+module Whois
+  class Record
+    class Parser
+
+      # Parser for the whois.markmonitor.com server.
+      #
+      # @see Whois::Record::Parser::Example
+      #   The Example parser for the list of all available methods.
+      #
+      class WhoisCorporatedomainsCom < BaseIcannCompliant
+        self.scanner = Scanners::BaseIcannCompliant, {
+            pattern_available: /^No match for/
+        }
+      end
+
+    end
+  end
+end
