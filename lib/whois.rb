f983fcb8fe01ea30c77bac3d7e3ec8485646e5a7
Jean Boussier <jean.boussier@gmail.com>
2014-03-30 13:33:36 -0400

    Actually remove_method is not used.

--- lib/whois.rb
+++ lib/whois.rb
@@ -11,7 +11,6 @@ require 'active_support/core_ext/array/extract_options'
 require 'active_support/core_ext/array/wrap'
 require 'active_support/core_ext/class/attribute'
 require 'active_support/core_ext/kernel/singleton_class'
-require 'active_support/core_ext/module/remove_method'
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/time/calculations'
 
