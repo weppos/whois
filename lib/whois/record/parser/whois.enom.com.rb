c342b8cc0d19795c9ddae27e6a444c8706bb0e4a
Alex Aitken <alex@alexaitken.com>
2014-04-14 18:50:15 +0000

    Add graceful handling for blank 'Updated Date' from whois.enom.com
    Fixes: https://github.com/weppos/whois/issues/320

--- lib/whois/record/parser/whois.enom.com.rb
+++ lib/whois/record/parser/whois.enom.com.rb
@@ -23,6 +23,12 @@ module Whois
         self.scanner = Scanners::BaseIcannCompliant, {
             pattern_available: /^Domain not found\.\n/
         }
+
+        property_supported :updated_on do
+          node('Updated Date') do |value|
+            parse_time(value) unless value.empty?
+          end
+        end
       end
 
     end
