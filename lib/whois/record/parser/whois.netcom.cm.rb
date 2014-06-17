8c85cebdac5c641e7f513440c609747c3411bf1d
Simone Carletti <weppos@weppos.net>
2014-06-17 15:37:16 +0200

    Change GH-333 to reuse the status_mapping

--- lib/whois/record/parser/whois.netcom.cm.rb
+++ lib/whois/record/parser/whois.netcom.cm.rb
@@ -17,38 +17,9 @@ module Whois
       # Parser for the whois.netcom.cm server.
       class WhoisNetcomCm < BaseCocca
 
-        property_supported :status do
-          if content_for_scanner =~ /Status:\s+(.+?)\n/
-            case $1.downcase
-            when "active"
-              :registered
-            when "suspended"
-              :registered
-            when "not registered"
-              :available
-            else
-              Whois.bug!(ParserError, "Unknown status `#{$1}'.")
-            end
-          else
-            Whois.bug!(ParserError, "Unable to parse status.")
-          end
-        end
-
-        property_supported :available? do
-          status == :available
-        end
-
-        property_supported :registered? do
-          !available?
-        end
-
-        property_supported :nameservers do
-          if content_for_scanner =~ /Name Servers:\n((.+\n)+)\n/
-            $1.split("\n").map do |name|
-              Record::Nameserver.new(:name => name.strip)
-            end
-          end
-        end
+        self.status_mapping.merge!({
+            "suspended" => :registered
+        })
 
       end
 
