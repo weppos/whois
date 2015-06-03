7573295cb85ea4b980439aa6509a03fa80fba8aa
Sheldon Hearn <sheldonh@starjuice.net>
2015-06-03 14:27:53 +0200

    Update support for .org.za
    It now falls under ZA Central Registry, served by org-whois.registry.net.za
    (although whois.org.za remains as an alias). The format matches the other
    ZA Central Registry WHOIS services.

---
+++ lib/whois/record/parser/org-whois.registry.net.za.rb
@@ -0,0 +1,28 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2014 Simone Carletti <weppos@weppos.net>
+#++
+
+
+require 'whois/record/parser/za_central_egistry'
+
+
+module Whois
+  class Record
+    class Parser
+
+      #
+      # = whois.org.za parser
+      #
+      # Parser for the whois.org.za server.
+      #
+      class OrgWhoisRegistryNetZa < ZaCentralRegistry
+
+      end
+
+    end
+  end
+end
