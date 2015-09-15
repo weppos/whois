0bb03de00ee0af69f0fff94f7b3409441bdd85ee
Simone Carletti <weppos@weppos.net>
2015-09-15 23:26:38 +0200

    Copyright Year

--- lib/whois/record/parser/whois.registro.br.rb
+++ lib/whois/record/parser/whois.registro.br.rb
@@ -3,7 +3,7 @@
 #
 # An intelligent pure Ruby WHOIS client and parser.
 #
-# Copyright (c) 2009-2014 Simone Carletti <weppos@weppos.net>
+# Copyright (c) 2009-2015 Simone Carletti <weppos@weppos.net>
 #++
 
 
