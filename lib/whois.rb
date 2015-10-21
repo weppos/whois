755dfe38aaef892f609ae54aaca5e0991468fc11
Simone Carletti <weppos@weppos.net>
2015-10-21 22:53:11 +0200

    Move Parser methods to the Ruby::Parser lib

--- lib/whois.rb
+++ lib/whois.rb
@@ -44,58 +44,6 @@ module Whois
 
     alias_method :whois, :lookup
 
-    # Checks whether the object represented by <tt>object</tt> is available.
-    #
-    # Warning: this method is only available if a Whois parser exists
-    # for the top level domain of <tt>object</tt>.
-    # If no parser exists for <tt>object</tt>, you'll receive a
-    # warning message and the method will return <tt>nil</tt>.
-    # This is a technical limitation. Browse the lib/whois/record/parsers
-    # folder to view all available parsers.
-    #
-    # @param  [String] object The string to be sent as query parameter.
-    #         It is intended to be a domain name, otherwise this method
-    #         may return unexpected responses.
-    # @return [Boolean]
-    #
-    # @example
-    #   Whois.available?("google.com")
-    #   # => false
-    #
-    # @example
-    #   Whois.available?("google-is-not-available-try-again-later.com")
-    #   # => true
-    #
-    def available?(object)
-      # ParserRequired
-    end
-
-    # Checks whether the object represented by <tt>object</tt> is registered.
-    #
-    # Warning: this method is only available if a Whois parser exists
-    # for the top level domain of <tt>object</tt>.
-    # If no parser exists for <tt>object</tt>, you'll receive a warning message
-    # and the method will return <tt>nil</tt>.
-    # This is a technical limitation. Browse the lib/whois/record/parsers folder
-    # to view all available parsers.
-    #
-    # @param  [String] object The string to be sent as query parameter.
-    #         It is intended to be a domain name, otherwise this method
-    #         may return unexpected responses.
-    # @return [Boolean]
-    #
-    # @example
-    #   Whois.registered?("google.com")
-    #   # => true
-    #
-    # @example
-    #   Whois.registered?("google-is-not-available-try-again-later.com")
-    #   # => false
-    #
-    def registered?(object)
-      # ParserRequired
-    end
-
 
     # Echoes a deprecation warning message.
     #
