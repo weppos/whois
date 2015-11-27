fa609dc4214a7a53b6341ca50aee61a1c2dcb5f8
gwire <gwire@users.noreply.github.com>
2015-11-27 02:24:35 +0000

    Update whois.ja.net parser to include registrant

--- lib/whois/record/parser/whois.ja.net.rb
+++ lib/whois/record/parser/whois.ja.net.rb
@@ -25,6 +25,46 @@ module Whois
       #
       class WhoisJaNet < Base
 
+        property_supported :domain do
+          if content_for_scanner =~ /^Domain:\n\s+(.+?)\n/
+            $1.strip.downcase
+          end
+        end
+
+        # @see https://community.jisc.ac.uk/library/janet-services-documentation/list-registrars Jisc approved registrars
+        property_supported :registrar do
+          Record::Registrar.new(
+            :id           => nil,
+            :name         => content_for_scanner[/Registered By:\n\s*(.+)\n/, 1],
+            :organization => content_for_scanner[/Registered By:\n\s*(.+)\n/, 1],
+            :url          => nil
+          )
+        end
+
+        # @note Address formatting is not reliable enough for simple parsing.
+        #   Treating "Domain Owner" as the registrant organisation
+        property_supported :registrant_contacts do
+          address = phone = fax = email = nil
+          textblock = content.slice(/Registrant Address:\n((.+\n)+)\n/, 1).gsub(/\t/,'')
+          phone = textblock.slice(/\s+(\+.+) \(Phone\)/, 1)
+          fax   = textblock.slice(/\s+(\+.+) \(FAX\)\n/, 1)
+          email = textblock.slice(/\s+(.*@.*)\n/, 1)
+          address = textblock.gsub(/(.*@.*|.* \((Phone|FAX)\)\n)/,'').strip
+          Record::Contact.new(
+              :type => Record::Contact::TYPE_REGISTRANT,
+              :organization => content_for_scanner[/Domain Owner:\n\s*(.+)\n/, 1],
+              :name => content_for_scanner[/Registrant Contact:\n\s*(.+)\n/, 1],
+              :address => address,
+              :city => nil,
+              :state => nil,
+              :zip => nil,
+              :country => nil,
+              :phone => phone,
+              :fax => fax,
+              :email => email
+          )
+        end
+
         property_supported :status do
           if available?
             :available
@@ -64,11 +104,19 @@ module Whois
         property_supported :nameservers do
           if content_for_scanner =~ /Servers:\n((.+\n)+)\n/
             $1.split("\n").map do |line|
-              name, ipv4 = line.strip.downcase.split("\t")
-              Record::Nameserver.new(:name => name, :ipv4 => ipv4)
+              name, ips = line.strip.downcase.split("\t")
+              ipv4 = [ips].find { |ip| Whois::Server.valid_ipv4?(ip) }
+              ipv6 = [ips].find { |ip| Whois::Server.valid_ipv6?(ip) }
+              Record::Nameserver.new(:name => name, :ipv4 => ipv4, :ipv6 => ipv6)
             end
           end
         end
+
+        property_not_supported :disclaimer
+
+        property_not_supported :admin_contacts
+
+        property_not_supported :technical_contacts
 
       end
 
