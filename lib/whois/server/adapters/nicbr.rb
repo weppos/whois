980e596900e61420893a198658ccd56418b8b659
mahtin <mahtin@mahtin.com>
2016-07-21 03:43:30 -0700

    Added additional support for nic.br via whois.gtlds.nic.br

---
+++ lib/whois/server/adapters/nicbr.rb
@@ -0,0 +1,51 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2016 Simone Carletti <weppos@weppos.net>
+#++
+
+
+module Whois
+  class Server
+    module Adapters
+
+      #
+      # = NICbr Adapter
+      #
+      # Provides ability to query NICbr WHOIS interfaces.
+      #
+      class Nicbr < Base
+
+        # Executes a WHOIS query to the NICbr WHOIS interface,
+        # resolving any intermediate referral,
+        # and appends the response to the client buffer.
+        #
+        # @param  [String] string
+        # @return [void]
+        #
+        def request(string)
+          response = query_the_socket(string, host)
+          buffer_append response, host
+
+          if options[:referral] != false && referral = extract_referral(response)
+            response = query_the_socket(string, referral)
+            buffer_append(response, referral)
+          end
+        end
+
+
+        private
+
+        def extract_referral(response)
+          if response =~ /Domain Name:/
+            response.slice(/Whois Server:(\S+)/, 1)
+          end
+        end
+
+      end
+
+    end
+  end
+end
