bb8a1f730a3ac48dbb53866716ad8336ce934abf
Simone Carletti <weppos@weppos.net>
2020-01-29 10:27:48 +0100

    Fix cops

--- lib/whois/server/adapters/arpa.rb
+++ lib/whois/server/adapters/arpa.rb
@@ -24,25 +24,25 @@ module Whois
 
         private
 
-          # "127.1.168.192.in-addr.arpa" => "192.168.1.127"
-          # "1.168.192.in-addr.arpa" => "192.168.1.0"
-          # "168.192.in-addr.arpa" => "192.168.0.0"
-          # "192.in-addr.arpa" => "192.0.0.0"
-          # "in-addr.arpa" => "0.0.0.0"
-          def inaddr_to_ip(string)
-            unless string.match?(/^([0-9]{1,3}\.?){0,4}in-addr\.arpa$/)
-              raise ServerError, "Invalid .in-addr.arpa address"
-            end
-             a, b, c, d = string.scan(/[0-9]{1,3}\./).reverse
-            [a, b, c, d].map do |token|
-              token = (token ||= 0).to_i
-              if token <= 255 && token >= 0
-                token
-              else
-                raise ServerError, "Invalid .in-addr.arpa token `#{token}'"
-              end
-            end.join(".")
+        # "127.1.168.192.in-addr.arpa" => "192.168.1.127"
+        # "1.168.192.in-addr.arpa" => "192.168.1.0"
+        # "168.192.in-addr.arpa" => "192.168.0.0"
+        # "192.in-addr.arpa" => "192.0.0.0"
+        # "in-addr.arpa" => "0.0.0.0"
+        def inaddr_to_ip(string)
+          unless string.match?(/^([0-9]{1,3}\.?){0,4}in-addr\.arpa$/)
+            raise ServerError, "Invalid .in-addr.arpa address"
           end
+          a, b, c, d = string.scan(/[0-9]{1,3}\./).reverse
+          [a, b, c, d].map do |token|
+            token = (token ||= 0).to_i
+            if token <= 255 && token >= 0
+              token
+            else
+              raise ServerError, "Invalid .in-addr.arpa token `#{token}'"
+            end
+          end.join(".")
+        end
 
       end
 
