e9c362e9a307c1ed3cdbc9d3170d446aa177680c
Simone Carletti <weppos@weppos.net>
2022-03-15 16:58:42 +0100

    Update year

--- lib/whois/server/adapters/arpa.rb
+++ lib/whois/server/adapters/arpa.rb
@@ -5,7 +5,7 @@
 #
 # An intelligent pure Ruby WHOIS client and parser.
 #
-# Copyright (c) 2009-2021 Simone Carletti <weppos@weppos.net>
+# Copyright (c) 2009-2022 Simone Carletti <weppos@weppos.net>
 #++
 
 
